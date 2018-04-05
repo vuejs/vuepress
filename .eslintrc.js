2a6fe420dc6fbd25a48e0fc6d21c53ff739196c8
Evan You <yyx990803@gmail.com>
2018-04-05 17:13:46 -0400

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue-libs/recommended']
+  extends: ['plugin:vue-libs/recommended'],
+  rules: {
+    indent: ['error', 2, { MemberExpression: 'off' }]
+  }
 }
