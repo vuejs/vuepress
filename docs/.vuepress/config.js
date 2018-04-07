1e74c9952cf0721b723a7b536318be21d1ca7c75
Evan You <yyx990803@gmail.com>
2018-04-07 15:18:31 -0400

    fix deployment for github pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   title: 'VuePress',
   dest: 'vuepress',
-  description: 'Minimalistic docs generator with Vue component based layout system'
+  description: 'Minimalistic docs generator with Vue component based layout system',
+}
+
+// set base URL if building for github
+if (process.env.GH) {
+  Object.assign(module.exports, {
+    base: '/vuepress/'
+  })
 }
---
+++ scripts/deploy-gh.sh
@@ -0,0 +1,5 @@
+cd vuepress
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:vuejs/vuepress.git master:gh-pages
