f4cf3b404edd9c86641baf35c235e9251234c3a8
Evan You <yyx990803@gmail.com>
2018-04-10 10:46:05 -0400

    refactor: avoid loading bar if page has been loaded

--- lib/default-theme/Layout.vue
+++ lib/default-theme/Layout.vue
@@ -7,10 +7,12 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import nprogress from 'nprogress'
 import Navbar from './Navbar.vue'
 import Page from './Page.vue'
 import Sidebar from './Sidebar.vue'
+import { pathToComponentName } from '../app/util'
 
 export default {
   components: { Page, Sidebar, Navbar },
@@ -18,7 +20,7 @@ export default {
     nprogress.configure({ showSpinner: false })
 
     this.$router.beforeEach((to, from, next) => {
-      if (to.path !== from.path) {
+      if (to.path !== from.path && !Vue.component(pathToComponentName(to.path))) {
         nprogress.start()
       }
       next()
