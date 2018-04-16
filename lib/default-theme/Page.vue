24fff7b20bf28b80afd83ae9436fc3f6eb33e81a
Evan You <yyx990803@gmail.com>
2018-04-16 16:20:50 -0400

    tweak: adjust page bottom padding

--- lib/default-theme/Page.vue
+++ lib/default-theme/Page.vue
@@ -109,17 +109,20 @@ function find (page, items, offset) {
 <style lang="stylus">
 @import './styles/config.styl'
 
-.edit-link
+.page
+  padding-bottom 2rem
+
+.edit-link.content
   padding-top 0 !important
   a
     color lighten($textColor, 25%)
     margin-right 0.25rem
 
 .page-nav.content
-  min-height 2.2rem
-  padding-bottom 2rem
-  padding-top 0.5rem !important
+  padding-top 1rem !important
+  padding-bottom 0 !important
   .inner
+    min-height 2rem
     margin-top 0 !important
     border-top 1px solid $borderColor
     padding-top 1rem
