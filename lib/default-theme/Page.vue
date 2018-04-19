cdf50949f132cc82617badaebec507f450baef6a
wangyi7099 <wy564654798@126.com>
2018-04-19 21:26:13 +0800

    Update Page.vue

--- lib/default-theme/Page.vue
+++ lib/default-theme/Page.vue
@@ -66,7 +66,7 @@ export default {
       }
 
       if (docsRepo && editLinks) {
-        const base = outboundRE.test(docRepo)
+        const base = outboundRE.test(docsRepo)
           ? docsRepo
           : `https://github.com/${docsRepo}`
         return (
