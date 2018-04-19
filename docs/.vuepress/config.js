d4f1af905fcca293de59ffcb5ae6e1754b351523
Gauthier de Girodon Pralong <dgpgdev@gmail.com>
2018-04-19 19:42:47 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,66 +1,81 @@
 module.exports = {
   dest: 'vuepress',
-  sitemap: {
-    domain: 'http://test.com'
-  },
-  base: '/base-doc/',
-  langs: [
-    { lang: 'en', label: 'English', path: '/', selectText: 'Languages' },
-    { lang: 'zh-CN', label: '简体中文', path: '/zh/', selectText: '选择语言' }
-  ],
-  title: {
-    '/': 'VuePress',
-    '/zh/': 'VuePress'
-  },
-  description: {
-    '/': 'Vue-powered Static Site Generator',
-    '/zh/': 'Vue 驱动的静态网站生成器'
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'VuePress',
+      description: 'Vue-powered Static Site Generator'
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'VuePress',
+      description: 'Vue 驱动的静态网站生成器'
+    }
   },
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }]
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   serviceWorker: true,
   themeConfig: {
     repo: 'vuejs/vuepress',
     editLinks: true,
     docsDir: 'docs',
-    nav: {
-      '/': [
-        {
-          text: 'Guide',
-          link: '/guide/',
-        },
-        {
-          text: 'Config Reference',
-          link: '/config/'
-        },
-        {
-          text: 'Default Theme Config',
-          link: '/default-theme-config/'
+    locales: {
+      '/': {
+        label: 'English',
+        selectText: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        nav: [
+          {
+            text: 'Guide',
+            link: '/guide/',
+          },
+          {
+            text: 'Config Reference',
+            link: '/config/'
+          },
+          {
+            text: 'Default Theme Config',
+            link: '/default-theme-config/'
+          }
+        ],
+        sidebar: {
+          '/guide/': genSidebarConfig('Guide')
         }
-      ],
-      '/zh/': [
-        {
-          text: '指南',
-          link: '/zh/guide/',
-        },
-        {
-          text: '配置',
-          link: '/zh/config/'
-        },
-        {
-          text: '默认主题',
-          link: '/zh/default-theme-config/'
+      },
+      '/zh/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '在 GitHub 上编辑此页',
+        nav: [
+          {
+            text: '指南',
+            link: '/zh/guide/',
+          },
+          {
+            text: '配置',
+            link: '/zh/config/'
+          },
+          {
+            text: '默认主题',
+            link: '/zh/default-theme-config/'
+          }
+        ],
+        sidebar: {
+          '/zh/guide/': genSidebarConfig('指南')
         }
-      ]
-    },
-    sidebar: {
-      '/guide/': genSidebarConfig('Guide'),
-      '/zh/guide/': genSidebarConfig('指南')
-    },
+      }
+    }
   }
 }
-
 
 function genSidebarConfig (title) {
   return [
@@ -75,6 +90,7 @@ function genSidebarConfig (title) {
         'markdown',
         'using-vue',
         'custom-themes',
+        'i18n',
         'deploy'
       ]
     }
