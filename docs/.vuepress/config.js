e9fcfccda44b0095b5dffb8a4efdf54aa47b47be
ULIVZ <472590061@qq.com>
2018-04-19 23:24:13 +0800

    chore: recover the default config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,7 +81,7 @@ function genSidebarConfig (title) {
   return [
     {
       title,
-      collapsable: true,
+      collapsable: false,
       children: [
         '',
         'getting-started',
