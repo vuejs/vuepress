4d66bb0a6c46cf2a44c3e29796391a830f16091e
Evan You <yyx990803@gmail.com>
2018-04-24 12:07:51 -0400

    tweak: add home link on 404 page

--- lib/default-theme/NotFound.vue
+++ lib/default-theme/NotFound.vue
@@ -3,6 +3,7 @@
     <div class="content">
       <h1>404</h1>
       <blockquote>{{ getMsg() }}</blockquote>
+      <router-link to="/">Take me home.</router-link>
     </div>
   </div>
 </template>
