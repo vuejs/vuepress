72db9f8f0f32d912c90ddb72b14b9578df61c749
ULIVZ <472590061@qq.com>
2018-04-24 23:16:53 +0800

    feat: handle life cycle

--- docs/.vuepress/vuepress-plugin-custom-domain.js
+++ docs/.vuepress/vuepress-plugin-custom-domain.js
@@ -1,8 +1,23 @@
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs-extra')
 
-module.exports = async function vuepressPluginCustomDomain({ outDir, pluginOptions: { domain } }) {
-  if (process.env.NODE_ENV === 'production') {
+module.exports = async function vuepressPluginCustomDomain({ domain }) {
+  this
+
+  .ready(() => {
+    console.log('Used vuepress-plugin-custom-domain')
+  })
+
+  .compiled(async ({ outDir }) => {
+    if (process.env.NODE_ENV !== 'production') {
+      return
+    }
+    await fs.ensureDir(outDir)
     await fs.writeFile(path.resolve(outDir, 'CHAME'), domain, 'utf-8')
-  }
+    console.log('Generated CHAME file.')
+  })
+
+  .updated(() => {
+    console.log('Updated')
+  })
 }
