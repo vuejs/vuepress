70f47792b2f4f60711f27b06d4a0e78efc4ec2d2
Jason Yu <me@ycmjason.com>
2018-05-09 17:59:23 +0100

    not done

---
+++ lib/default-theme/ScrollTop.vue
@@ -0,0 +1,45 @@
+<template>
+  <main v-if="show">
+    <button>Go To Top</button>
+  </main>
+</template>
+
+<script>
+import debounce from 'lodash.debounce'
+
+export default {
+  props: {
+    threshold: {
+      type: Number,
+      default
+    }
+  },
+
+  data: () => ({
+    isShow: false
+  }),
+  mounted () {
+    window.addEventListener('scroll', debounce(() => {
+      if (this.getScrollTop() < 300) return this.hide()
+      this.show()
+    }, 300))
+  },
+  methods: {
+    getScrollTop () {
+      return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+    },
+
+    show () {
+      this.isShow = true
+    },
+
+    hide () {
+      this.isShow = false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
