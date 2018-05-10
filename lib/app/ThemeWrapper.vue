707fc3e033671df6e376de3a7d3d38802bfae31a
Xinyu Liu <meteor.lxy@foxmail.com>
2018-05-10 09:19:10 +0800

    fix: move ssrContext into theme wrapper

---
+++ lib/app/ThemeWrapper.vue
@@ -0,0 +1,62 @@
+<template>
+  <ThemeLayout v-if="$page.path"/>
+
+  <ThemeNotFound v-else/>
+</template>
+
+<script>
+import ThemeLayout from '@themeLayout'
+import ThemeNotFound from '@themeNotFound'
+export default {
+  components: {
+    ThemeLayout,
+    ThemeNotFound
+  },
+  created () {
+    if (this.$ssrContext) {
+      this.$ssrContext.title = this.$title
+      this.$ssrContext.lang = this.$lang
+      this.$ssrContext.description = this.$page.description || this.$description
+    }
+  },
+  mounted () {
+    this.currentMetaTags = []
+    const updateMeta = () => {
+      document.title = this.$title
+      document.documentElement.lang = this.$lang
+      const meta = [
+        {
+          name: 'description',
+          content: this.$description
+        },
+        ...(this.$page.frontmatter.meta || [])
+      ]
+      this.currentMetaTags = updateMetaTags(meta, this.currentMetaTags)
+    }
+    this.$watch('$page', updateMeta)
+    updateMeta()
+  },
+  beforeDestroy () {
+    updateMetaTags(null, this.currentMetaTags)
+  }
+}
+
+function updateMetaTags (meta, current) {
+  if (current) {
+    current.forEach(c => {
+      document.head.removeChild(c)
+    })
+  }
+  if (meta) {
+    return meta.map(m => {
+      const tag = document.createElement('meta')
+      Object.keys(m).forEach(key => {
+        tag.setAttribute(key, m[key])
+      })
+      document.head.appendChild(tag)
+      return tag
+    })
+  }
+}
+</script>
+
