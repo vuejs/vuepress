e1a4f5cc88423c822a03387f0c356cf5f8a128f5
ULIVZ <472590061@qq.com>
2018-05-23 03:21:10 +0800

    chore: tweaks

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,81 @@
 module.exports = {
   dest: 'vuepress',
-  lang: 'en-US',
-  title: 'VuePress',
-  description: 'Vue-powered Static Site Generator',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'VuePress',
+      description: 'Vue-powered Static Site Generator'
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'VuePress',
+      description: 'Vue 驱动的静态网站生成器'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  serviceWorker: true,
+  theme: 'vue',
   themeConfig: {
     repo: 'vuejs/vuepress',
     editLinks: true,
     docsDir: 'docs',
-    label: 'English',
-    selectText: 'Languages',
-    editLinkText: 'Edit this page on GitHub',
-    lastUpdated: 'Last Updated',
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
+    locales: {
+      '/': {
+        label: 'English',
+        selectText: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+        nav: [
+          {
+            text: 'Guide',
+            link: '/guide/',
+          },
+          {
+            text: 'Config Reference',
+            link: '/config/'
+          },
+          {
+            text: 'Default Theme Config',
+            link: '/default-theme-config/'
+          }
+        ],
+        sidebar: {
+          '/guide/': genSidebarConfig('Guide')
+        }
       },
-      {
-        text: 'Config Reference',
-        link: '/config/'
-      },
-      {
-        text: 'Default Theme Config',
-        link: '/default-theme-config/'
+      '/zh/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        nav: [
+          {
+            text: '指南',
+            link: '/zh/guide/',
+          },
+          {
+            text: '配置',
+            link: '/zh/config/'
+          },
+          {
+            text: '默认主题',
+            link: '/zh/default-theme-config/'
+          }
+        ],
+        sidebar: {
+          '/zh/guide/': genSidebarConfig('指南')
+        }
       }
-    ],
-    sidebar: {
-      '/guide/': genSidebarConfig('Guide')
     }
   }
 }
