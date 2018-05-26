21e8757966718b32ed6937defa0da0b582f85d80
ULIVZ <472590061@qq.com>
2018-05-27 00:54:33 +0800

    test: test: markdown - hoist plugin

---
+++ test/markdown/fragments/hoist.md
@@ -0,0 +1,10 @@
+# H1
+
+<script src="vue.js"></script>
+<style>
+  .vue {
+    font-size: 16px;
+  }
+</style>
+
+## H2
--- test/markdown/util.js
+++ test/markdown/util.js
@@ -1,15 +1,24 @@
 import fs from 'fs-extra'
+import LRU from 'lru-cache'
 import path from 'path'
 
+const cache = LRU({ max: 1000 })
+
 export function Md () {
-  return require('markdown-it')({
+  const md = require('markdown-it')({
     html: true,
     highlight: require('@/markdown/highlight.js')
   })
+  return md
 }
 
 export async function getFragment (name) {
+  let content = cache.get(name)
+  if (content) {
+    return content
+  }
   const target = path.resolve(__dirname, `fragments/${name}.md`)
-  const content = await fs.readFile(target, 'utf-8')
+  content = await fs.readFile(target, 'utf-8')
+  cache.set(name, content)
   return content
 }
