0dd6cacf44aefd02a6303df53755ea1b4e948197
Alexander Sokolov <modex13@gmail.com>
2018-05-26 17:51:51 +0300

    Update DropdownLink.vue

--- lib/default-theme/DropdownLink.vue
+++ lib/default-theme/DropdownLink.vue
@@ -95,7 +95,7 @@ export default {
             border-top 3px solid transparent
             border-bottom 3px solid transparent
             position absolute
-            top calc(1rem - 2px)
+            top calc(1rem - 5px)
             left 9px
       &:first-child h4
         margin-top 0
