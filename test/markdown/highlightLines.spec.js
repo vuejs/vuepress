f593ce7f27fcf1c6be7f0d84e6d56b7c76aedd2d
ULIVZ <472590061@qq.com>
2018-05-27 00:19:28 +0800

    chore: tweaks

--- test/markdown/highlightLines.spec.js
+++ test/markdown/highlightLines.spec.js
@@ -2,25 +2,25 @@ import { Md, getFragment } from './util'
 import highlightLines from '@/markdown/highlightLines.js'
 
 const md = Md()
-const mdh = Md().use(highlightLines)
+const mdH = Md().use(highlightLines)
 
 describe('highlightLines', () => {
   test('should the output not change when highlightLines is not detected', async () => {
     const input = await getFragment('code')
     const output1 = md.render(input)
-    const output2 = mdh.render(input)
+    const output2 = mdH.render(input)
     expect(output1).toBe(output2)
   })
 
   test('highlight single line', async () => {
     const input = await getFragment('code-highlightLines-single')
-    const output = mdh.render(input)
+    const output = mdH.render(input)
     expect(output).toMatchSnapshot()
   })
 
   test('highlight multiple lines', async () => {
     const input = await getFragment('code-highlightLines-multiple')
-    const output = mdh.render(input)
+    const output = mdH.render(input)
     expect(output).toMatchSnapshot()
   })
 })
