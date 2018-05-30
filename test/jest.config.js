f9c391404347db42af615c62b9940435e0a05eae
Liu Xinyu <meteor.lxy@foxmail.com>
2018-05-30 09:34:54 -0500

    chore: set rootDir in jest config (#528)

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,20 +1,23 @@
 // https://github.com/facebook/jest/tree/master/packages/babel-jest
 // TODO remove 'babel-core@^7.0.0-0' when babel-jest can work with '@babel/core'
 
+const path = require('path')
+
 module.exports = {
+  rootDir: path.resolve(__dirname, '..'),
   verbose: true,
   moduleFileExtensions: [
     'js',
     'vue'
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/../lib/$1'
+    '^@/(.*)$': '<rootDir>/lib/$1'
   },
   transform: {
-    '^.+\\.js$': '<rootDir>/../node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/../node_modules/vue-jest'
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   snapshotSerializers: [
-    '<rootDir>/../node_modules/jest-serializer-vue'
+    '<rootDir>/node_modules/jest-serializer-vue'
   ]
 }
