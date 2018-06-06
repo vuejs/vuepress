dd87d37008d976a047748eb1d0a4d8319e1e6a47
ULIVZ <472590061@qq.com>
2018-06-06 21:19:13 +0800

    feat: support 'clientRootMixin' option and change 'active-header-links' as a plugin.

--- packages/vuepress-plugin-active-header-links/index.js
+++ packages/vuepress-plugin-active-header-links/index.js
@@ -2,9 +2,6 @@ const path = require('path')
 
 module.exports = (options, context) => ({
   name: 'active-header-links',
-
-  client: path.resolve(__dirname, 'client.js'),
-
-  // This file will be mixed into the root component of each page.
-  clientRootMixin: path.resolve(__dirname, 'root-mixin.js')
+  // This mixin will be applied to the root component of each page.
+  clientRootMixin: path.resolve(__dirname, 'mixin.js')
 })
