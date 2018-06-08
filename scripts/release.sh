19368c412850087cda515e3f66c6063676841390
ULIVZ <472590061@qq.com>
2018-06-09 01:36:50 +0800

    workflow: add release script.

---
+++ scripts/release.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Select a version to release (input number)："
+echo
+
+select VERSION in patch minor major "Specific Version"
+  do
+    echo
+    if [[ $REPLY == 4 ]]; then
+      read -p "Enter a specific version: " -r VERSION
+      echo
+      if [[ -z $REPLY ]]; then
+        VERSION=$REPLY
+      fi
+    fi
+
+    read -p "Releasing @$VERSION version - are you sure? (y/n) " -n 1 -r
+    echo
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      # pre release task
+      npm run lint
+      npm run test
+
+      # bump version
+      npm version $VERSION
+      NEW_VERSION=$(node -p "require('./package.json').version")
+      echo Releasing ${NEW_VERSION} ...
+
+      # github release
+      npm run prepublishOnly
+      git add CHANGELOG.md
+      git commit -m "chore: changelog"
+      git push
+      git push origin refs/tags/v${NEW_VERSION}
+      echo "✅ released to Github"
+
+      # npm release
+      npm publish
+      echo "✅ released to npm"
+    else
+      echo Cancelled
+    fi
+    break
+  done
+
