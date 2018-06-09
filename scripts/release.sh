c634e63674e21c40e74b0775c5f888b39ba1da8f
ULIVZ <472590061@qq.com>
2018-06-09 18:50:09 +0800

    refactor($release): add validation for release option's reply

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
-echo "Select a version to release (input number)："
+echo "Select a option to release (input a serial number)："
 echo
 
 select VERSION in patch minor major "Specific Version"
   do
     echo
-    if [[ $REPLY == 4 ]]; then
-      read -p "Enter a specific version: " -r VERSION
-      echo
-      if [[ -z $REPLY ]]; then
-        VERSION=$REPLY
+    if [[ $REPLY =~ ^[1-4]$ ]]; then
+      if [[ $REPLY == 4 ]]; then
+        read -p "Enter a specific version: " -r VERSION
+        echo
+        if [[ -z $REPLY ]]; then
+          VERSION=$REPLY
+        fi
       fi
-    fi
 
-    read -p "Releasing @$VERSION version - are you sure? (y/n) " -n 1 -r
-    echo
+      read -p "Release $VERSION - are you sure? (y/n) " -n 1 -r
+      echo
 
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      # pre release task
-      npm run lint
-      npm run test
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # pre release task
+        npm run lint
+        npm run test
 
-      # bump version
-      npm version $VERSION
-      NEW_VERSION=$(node -p "require('./package.json').version")
-      echo Releasing ${NEW_VERSION} ...
+        # bump version
+        npm version $VERSION
+        NEW_VERSION=$(node -p "require('./package.json').version")
+        echo Releasing ${NEW_VERSION} ...
 
-      # npm release
-      npm publish
-      echo "✅ released to npm"
+        # npm release
+        npm publish
+        echo "✅ Released to npm."
 
-      # github release
-      git add CHANGELOG.md
-      git commit -m "chore: changelog"
-      git push
-      git push origin refs/tags/v${NEW_VERSION}
-      echo "✅ released to Github"
+        # github release
+        git add CHANGELOG.md
+        git commit -m "chore: changelog"
+        git push
+        git push origin refs/tags/v${NEW_VERSION}
+        echo "✅ Released to Github."
+      else
+        echo Cancelled
+      fi
+      break
     else
-      echo Cancelled
+      echo Invalid \"${REPLY}\"
+      echo "To continue, please input a serial number(1-4) of an option."
+      echo
     fi
-    break
   done
 
