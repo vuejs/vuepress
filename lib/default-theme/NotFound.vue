4a578d16e675c19f277b034f71761830172350bd
ULIVZ <472590061@qq.com>
2018-06-15 01:20:48 +0800

    Revert "style: change ".content" to "#content""
    This reverts commit 4a4f8ed01cd54337d272f04d1ba9c378ffd7cff5.

--- lib/default-theme/NotFound.vue
+++ lib/default-theme/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="theme-container">
-    <div id="content">
+    <div class="content">
       <h1>404</h1>
       <blockquote>{{ getMsg() }}</blockquote>
       <router-link to="/">Take me home.</router-link>
