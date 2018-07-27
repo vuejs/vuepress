29e7fe456e0df7825fb2e9a3f12f698ff94d4c3a
ULIVZ <472590061@qq.com>
2018-07-27 19:29:40 +0800

    test: fix SecurityError: localStorage is not available for opaque origins
    ref: https://github.com/facebook/jest/issues/6769

--- test/jest.config.js
+++ test/jest.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 module.exports = {
   rootDir: path.resolve(__dirname, '..'),
   verbose: true,
+  testURL: 'http://localhost/',
   moduleFileExtensions: [
     'js',
     'vue'
