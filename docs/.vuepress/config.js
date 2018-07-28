92314ac2113f7c1df276b07e85e4dfe585bde81f
ULIVZ <472590061@qq.com>
2018-07-28 15:45:55 +0800

    docs: i18n for serviceWorker.updatePopup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,15 +29,18 @@ module.exports = {
     repo: 'vuejs/vuepress',
     editLinks: true,
     docsDir: 'docs',
-    serviceWorker: {
-      updatePopup: true
-    },
     locales: {
       '/': {
         label: 'English',
         selectText: 'Languages',
         editLinkText: 'Edit this page on GitHub',
         lastUpdated: 'Last Updated',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
         nav: [
           {
             text: 'Guide',
@@ -65,6 +68,12 @@ module.exports = {
         selectText: '选择语言',
         editLinkText: '在 GitHub 上编辑此页',
         lastUpdated: '上次更新',
+        serviceWorker: {
+          updatePopup: {
+            message: "发现新内容可用",
+            buttonText: "刷新"
+          }
+        },
         nav: [
           {
             text: '指南',
