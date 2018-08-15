2236365d1f5a828709cbaa2bc0f5dac08b818d44
ULIVZ <472590061@qq.com>
2018-08-15 23:07:35 +0800

    feat: using shortcut to resolve plugin

--- packages/@vuepress/core/lib/prepare/resolvePlugin.js
+++ packages/@vuepress/core/lib/prepare/resolvePlugin.js
@@ -2,10 +2,6 @@ const path = require('path')
 const Plugin = require('../plugin-api/index')
 const PluginContext = require('../plugin-api/context')
 
-const lastUpdatedPlugin = require('@vuepress/plugin-last-updated')
-const enhanceAppPlugin = require('@vuepress/plugin-enhance-app')
-const registerGlobalComponentsPlugin = require('@vuepress/plugin-register-components')
-
 module.exports = function (options) {
   const { siteConfig, themeConfig, sourceDir, themePath, themePlugins, cliPlugins } = options
   const pluginContext = new PluginContext(options)
@@ -17,8 +13,8 @@ module.exports = function (options) {
     .useByPluginsConfig(siteConfig.plugins)
     .useByPluginsConfig(themePlugins)
     // built-in plugins
-    .use(enhanceAppPlugin)
-    .use(registerGlobalComponentsPlugin, {
+    .use('@vuepress/enhance-app')
+    .use('@vuepress/register-components', {
       componentsDir: [
         path.resolve(sourceDir, '.vuepress/components'),
         path.resolve(themePath, 'components')
@@ -32,7 +28,7 @@ module.exports = function (options) {
       .some(base => themeConfig.locales[base].lastUpdated)
   )
   if (shouldUseLastUpdated) {
-    plugin.use(lastUpdatedPlugin)
+    plugin.use('@vuepress/last-updated')
   }
 
   return plugin
