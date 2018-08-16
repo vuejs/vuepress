662ce55f7f70a9d900eea098a7bbef8d2c22e5ba
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2018-08-16 13:05:04 +0200

    add draft implementation

---
+++ lib/app/components/ClipboardCopy.vue
@@ -0,0 +1,13 @@
+
+<template functional>
+  <svg class="icon copy-clipboard" style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
+</svg>
+</template>
+
+<style lang="stylus">
+.icon.copy-clipboard
+  color #aaa
+  display inline-block
+  cursor pointer
+</style>
