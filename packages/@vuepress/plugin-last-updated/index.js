1c70be60c97361b987e6a925aa3a486c423bdc33
ULIVZ <472590061@qq.com>
2018-08-17 03:05:40 +0800

    chore: tweaks naming

--- packages/@vuepress/plugin-last-updated/index.js
+++ packages/@vuepress/plugin-last-updated/index.js
@@ -1,14 +1,14 @@
 const spawn = require('cross-spawn')
 
 module.exports = (options = {}, context) => ({
-  extendPageData ({ filepath }) {
+  extendPageData ({ filePath }) {
     const { transformer } = options
-    const timestamp = getGitLastUpdatedTimeStamp(filepath)
+    const timestamp = getGitLastUpdatedTimeStamp(filePath)
     const lastUpdated = typeof transformer === 'function' ? transformer(timestamp) : timestamp
     return { lastUpdated }
   }
 })
 
-function getGitLastUpdatedTimeStamp (filepath) {
-  return parseInt(spawn.sync('git', ['log', '-1', '--format=%ct', filepath]).stdout.toString('utf-8')) * 1000
+function getGitLastUpdatedTimeStamp (filePath) {
+  return parseInt(spawn.sync('git', ['log', '-1', '--format=%ct', filePath]).stdout.toString('utf-8')) * 1000
 }
