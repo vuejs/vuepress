5350e364b0abd3bdb8aefcd4bff1f5c070ca9631
ULIVZ <472590061@qq.com>
2018-08-17 20:00:38 +0800

    test: improve test for plugin

--- packages/@vuepress/core/__test__/plugin-api/Plugin.spec.js
+++ packages/@vuepress/core/__test__/plugin-api/Plugin.spec.js
@@ -5,11 +5,51 @@ const Plugin = require('../../lib/plugin-api/index')
 const { PLUGIN_OPTION_MAP } = require('../../lib/plugin-api/constants')
 
 describe('Plugin', () => {
-  test('should resolve scope packages correctly.', () => {
+  test('should \'registerOption\' work.', () => {
     const plugin = new Plugin()
     const readyHandler = () => {}
     plugin.registerOption(PLUGIN_OPTION_MAP.READY.key, readyHandler)
     expect(plugin.options.ready.values).toHaveLength(1)
     expect(plugin.options.ready.values[0]).toBe(readyHandler)
+  })
+
+  test('should \'useByPluginsConfig\' work.', () => {
+    [
+      ['a'],
+      [['a']],
+      [['a', true]],
+      { a: true }
+    ].forEach(pluginsConfig => {
+      const plugin = new Plugin()
+      plugin.useByPluginsConfig(pluginsConfig)
+      expect(plugin.enabledPlugins).toHaveLength(1)
+      expect(plugin.enabledPlugins[0].name).toBe('vuepress-plugin-a')
+      expect(plugin.disabledPlugins).toHaveLength(0)
+    })
+  })
+
+  test('should \'useByPluginsConfig\' work. - disable plugin', () => {
+    [
+      [['a', false]],
+      { a: false }
+    ].forEach(pluginsConfig => {
+      const plugin = new Plugin()
+      plugin.useByPluginsConfig(pluginsConfig)
+      expect(plugin.enabledPlugins).toHaveLength(0)
+      expect(plugin.disabledPlugins).toHaveLength(1)
+      expect(plugin.disabledPlugins[0].name).toBe('vuepress-plugin-a')
+    })
+  })
+
+  test('should \'useByPluginsConfig\' work. - get options', () => {
+    const pluginOptions = {};
+    [
+      [['a', pluginOptions]],
+      { a: pluginOptions }
+    ].forEach(pluginsConfig => {
+      const plugin = new Plugin()
+      plugin.useByPluginsConfig(pluginsConfig)
+      expect(plugin.enabledPlugins[0].$$options).toBe(pluginOptions)
+    })
   })
 })
