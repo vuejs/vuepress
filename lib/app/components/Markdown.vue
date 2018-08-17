18b154cb025d6560af4209aaf705daab4a3409f3
许云峰 <xuyunfeng.fe@bytedance.com>
2018-08-17 22:54:20 +0800

    feat: Add Markdown component Used to reference another markdown file in a markdown file.

---
+++ lib/app/components/Markdown.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <component v-if="this.componentName" :is="this.componentName"></component>
+    </div>
+</template>
+
+<script>
+import { routes } from '../.temp/routes'
+import Vue from 'vue'
+
+export default {
+  props: {
+    src: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      componentName: '',
+      paths: []
+    }
+  },
+  async mounted () {
+    const page = routes.find(route => {
+      if (route.component != null) {
+        return route.path.replace('.html', '') === this.src
+      }
+    }) || {}
+    if (Vue.component(page.name) != null) {
+      this.componentName = page.name
+    }
+  }
+}
+</script>
