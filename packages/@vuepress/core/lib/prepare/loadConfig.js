04723be4efdad6398a5c9f2af77ab3675918ba1d
Eko Eryanto <36023898+ekoeryanto@users.noreply.github.com>
2018-09-06 08:19:06 +0700

    chore: change config load order (#804)

--- packages/@vuepress/core/lib/prepare/loadConfig.js
+++ packages/@vuepress/core/lib/prepare/loadConfig.js
@@ -14,12 +14,12 @@ module.exports = function loadConfig (vuepressDir, bustCache = true) {
 
   // resolve siteConfig
   let siteConfig = {}
-  if (fs.existsSync(configYmlPath)) {
-    siteConfig = parseConfig(configYmlPath)
+  if (fs.existsSync(configPath)) {
+    siteConfig = parseConfig(configPath)
   } else if (fs.existsSync(configTomlPath)) {
     siteConfig = parseConfig(configTomlPath)
-  } else if (fs.existsSync(configPath)) {
-    siteConfig = require(configPath)
+  } else if (fs.existsSync(configYmlPath)) {
+    siteConfig = require(configYmlPath)
   }
 
   return siteConfig
