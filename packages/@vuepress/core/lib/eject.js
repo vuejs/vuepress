62221702002ae1d3d4663668e72ca9af4b991cd6
Eko Eryanto <ekoeryanto@gmail.com>
2018-09-28 22:59:46 +0700

    fix: eject failed because config.styl not exist (#874)

--- packages/@vuepress/core/lib/eject.js
+++ packages/@vuepress/core/lib/eject.js
@@ -13,10 +13,5 @@ module.exports = async (dir) => {
   const source = require.resolve('@vuepress/theme-default')
   const target = path.resolve(dir, '.vuepress/theme')
   await fs.copy(source, target)
-  // remove the import to default theme override
-  const styleConfig = path.resolve(target, 'styles/config.styl')
-  const content = await fs.readFile(styleConfig, 'utf-8')
-  const transformed = content.split('\n').slice(0, -2).join('\n')
-  await fs.writeFile(styleConfig, transformed)
   logger.success(`\nCopied default theme into ${chalk.cyan(target)}.\n`)
 }
