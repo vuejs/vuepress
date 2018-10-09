b1c022e465339cafdfbb617d7b02c014e042ad96
许云峰 <xuyunfeng.fe@bytedance.com>
2018-10-09 12:06:26 +0800

    feat: export site pdf file

--- packages/@vuepress/plugin-export-site/export.js
+++ packages/@vuepress/plugin-export-site/export.js
@@ -1,62 +1,55 @@
 const puppeteer = require('puppeteer')
 const PDFMerge = require('easy-pdf-merge')
 const { path, fs, logger } = require('@vuepress/shared-utils')
-const prepare = require('../core/lib/prepare')
 
-module.exports = async ({ extension, sourceDir, siteOptions }) => {
+module.exports = async ({ extension, sourceDir, pages, dest }) => {
   if (extension === 'pdf') {
-    await exportPDF(sourceDir, siteOptions)
+    await exportPDF({ sourceDir, pages, dest })
   } else {
     logger.warn(`Not support ${extension} format site export!`)
   }
 }
 
-async function exportPDF (sourceDir, siteOptions = {}) {
-  if (!siteOptions.siteData) {
-    logger.error('no such options:siteOptions')
-    return
+async function exportPDF ({ sourceDir, pages = [], dest }) {
+  try {
+    const paths = pages.map(s => s.path)
+    const pdfTempDir = path.resolve('./_tempPDF')
+    fs.ensureDirSync(pdfTempDir)
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    // port finder
+    const options = paths.map(path => {
+      return {
+        location: `http://localhost:8080${path}`,
+        path: `${pdfTempDir}/${path.replace(/\//g, '-').replace('-', '').replace(/\.html/, '').replace(/-$/, '')}.pdf`
+      }
+    })
+    const files = options.map(option => path.resolve(option.path))
+    await downloadPDFs(page, options)
+    await mergePDF(files, dest)
+    await browser.close()
+    fs.removeSync(pdfTempDir)
+  } catch (e) {
+    logger.error(e.stack)
   }
-  siteOptions = await prepare(sourceDir)
-  const paths = siteOptions.siteData.pages.map(s => s.path)
-  const pdfTempDir = path.resolve('./_tempPDF')
-  if (!fs.pathExistsSync(pdfTempDir)) {
-    fs.mkdir(pdfTempDir)
-  }
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  const options = paths.map(path => {
-    return {
-      location: `http://localhost:8080${path}`,
-      path: `${pdfTempDir}/${path.replace(/\//g, '-').replace('-', '').replace(/\.html/, '').replace(/-$/, '')}.pdf`
-    }
-  })
-  const files = options.map(option => path.resolve(option.path))
-  await downloadPDFs(page, options)
-  await mergePDF(files, siteOptions.siteConfig.dest)
-  await browser.close()
-  fs.removeSync(pdfTempDir)
 }
 
 async function downloadPDFs (page, options) {
-  try {
-    for (let i = 0; i < options.length; i++) {
-      const { location, path } = options[i]
-      await page.goto(location)
-      await page.pdf({ path, format: 'A4' })
-      console.log(`pdf ${path} generator success`)
-    }
-  } catch (e) {
-    console.log(e)
+  for (let i = 0; i < options.length; i++) {
+    const { location, path } = options[i]
+    await page.goto(location, { waitUntil: 'networkidle2' })
+    await page.pdf({ path, format: 'A4' })
+    logger.success(`pdf ${path} generator success`)
   }
 }
 
 function mergePDF (files, exportFile = 'site') {
   return new Promise((resolve, reject) => {
-    PDFMerge(files, `./${exportFile}.pdf`, (err) => {
+    PDFMerge(files, `${exportFile}.pdf`, (err) => {
       if (err) {
         reject(err)
       }
-      console.log(`export ${exportFile} pdf file success!`)
+      logger.success(`export ${exportFile}.pdf file success!`)
       resolve()
     })
   })
--- packages/@vuepress/plugin-export-site/index.js
+++ packages/@vuepress/plugin-export-site/index.js
@@ -1,11 +1,17 @@
 const siteExport = require('./export')
 
-module.exports = (options, ctx) => {
+module.exports = (options = {}, ctx) => {
   return {
     // For production
-    async generated () {
-      console.log('generated!', ctx)
-      siteExport(options)
+    async ready () {
+      setTimeout(() => {
+        siteExport({
+          extension: options.extension,
+          sourceDir: ctx.sourceDir,
+          pages: ctx.pages,
+          dest: ctx.outDir
+        }, 0)
+      })
     }
   }
 }
