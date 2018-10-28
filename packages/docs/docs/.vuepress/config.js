70962d103178b11c68d44769dbfec4ec0af76ca0
Ho Chin Chee <alexho97@hotmail.com>
2018-10-29 04:54:45 +0800

    Update config.js typo

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -78,10 +78,10 @@ module.exports = {
   clientRootMixin: path.resolve(__dirname, 'mixin.js')
 }
 
-function getGuideSidebar (gruopA, groupB) {
+function getGuideSidebar (groupA, groupB) {
   return [
     {
-      title: gruopA,
+      title: groupA,
       collapsable: false,
       children: [
         '',
@@ -106,10 +106,10 @@ function getGuideSidebar (gruopA, groupB) {
   ]
 }
 
-function getPluginSidebar (gruopA, introductionA) {
+function getPluginSidebar (groupA, introductionA) {
   return [
     {
-      title: gruopA,
+      title: groupA,
       collapsable: false,
       children: [
         ['', introductionA],
@@ -124,10 +124,10 @@ function getPluginSidebar (gruopA, introductionA) {
   ]
 }
 
-function getThemeSidebar (gruopA, introductionA) {
+function getThemeSidebar (groupA, introductionA) {
   return [
     {
-      title: gruopA,
+      title: groupA,
       collapsable: false,
       children: [
         ['', introductionA],
