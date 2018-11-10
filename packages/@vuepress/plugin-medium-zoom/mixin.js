d8b310f575982581ccbd414743283aa01cc9a9ee
Daniel Castillo <dcastillok@gmail.com>
2018-11-10 22:00:36 +0100

    🐛 fix medium-zoom not updating on component update

--- packages/@vuepress/plugin-medium-zoom/mixin.js
+++ packages/@vuepress/plugin-medium-zoom/mixin.js
@@ -4,9 +4,22 @@ import './style.css'
 import zoom from 'medium-zoom'
 
 export default {
+  data: () => ({ zoom: null }),
+
   mounted () {
+    const self = this
     setTimeout(() => {
-      zoom(SELECTOR)
+      self.zoom = zoom(SELECTOR)
+    }, 1000)
+  },
+
+  updated () {
+    const self = this
+    setTimeout(() => {
+      if (self.zoom) {
+        self.zoom.detach()
+        self.zoom.attach(SELECTOR)
+      }
     }, 1000)
   }
 }
