ca03a1263c43b1e2063b945dbb2f1053a33153eb
ULIVZ <472590061@qq.com>
2018-11-14 00:38:27 +0800

    chore($markdown): hoist index.js to root dir

--- packages/@vuepress/markdown/__tests__/link.spec.js
+++ packages/@vuepress/markdown/__tests__/link.spec.js
@@ -1,6 +1,6 @@
 import { Md } from './util'
 import link from '../lib/link.js'
-import { dataReturnable } from '../lib/index.js'
+import { dataReturnable } from '../index.js'
 
 const mdL = Md().use(link, {
   target: '_blank',
