2a4e37f4ca483d50e313ea61dbefe82948b301b8
ULIVZ <472590061@qq.com>
2018-11-18 04:47:09 +0800

    chore($last-updated): ensure build process success if not in git repo. (close: #1007)
    During some CI context, the git command would not exist, this change just ensure that the build process does not crash.

--- packages/@vuepress/plugin-last-updated/index.js
+++ packages/@vuepress/plugin-last-updated/index.js
@@ -4,11 +4,17 @@ module.exports = (options = {}, context) => ({
   extendPageData ($page) {
     const { transformer } = options
     const timestamp = getGitLastUpdatedTimeStamp($page._filePath)
-    const lastUpdated = typeof transformer === 'function' ? transformer(timestamp) : timestamp
-    $page.lastUpdated = lastUpdated
+    if (timestamp) {
+      const lastUpdated = typeof transformer === 'function' ? transformer(timestamp) : timestamp
+      $page.lastUpdated = lastUpdated
+    }
   }
 })
 
 function getGitLastUpdatedTimeStamp (filePath) {
-  return parseInt(spawn.sync('git', ['log', '-1', '--format=%ct', filePath]).stdout.toString('utf-8')) * 1000
+  let lastUpdated
+  try {
+    lastUpdated = parseInt(spawn.sync('git', ['log', '-1', '--format=%ct', filePath]).stdout.toString('utf-8')) * 1000
+  } catch (e) { /* do not handle for now */ }
+  return lastUpdated
 }
