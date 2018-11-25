c059faa6fd19ee63ab8a9515c1b0cf349b805697
ULIVZ <472590061@qq.com>
2018-11-26 01:31:35 +0800

    fix($theme-default): shouldn't transform date string at client side (close: #1035)
    Move the transformation to $plugin-last-updated, BTW, expose `lang` as the 2nd argument

--- packages/@vuepress/plugin-last-updated/index.js
+++ packages/@vuepress/plugin-last-updated/index.js
@@ -4,12 +4,19 @@ module.exports = (options = {}, context) => ({
   extendPageData ($page) {
     const { transformer } = options
     const timestamp = getGitLastUpdatedTimeStamp($page._filePath)
+    const $lang = $page._computed.$lang
     if (timestamp) {
-      const lastUpdated = typeof transformer === 'function' ? transformer(timestamp) : timestamp
+      const lastUpdated = typeof transformer === 'function'
+        ? transformer(timestamp, $lang)
+        : defaultTransformer(timestamp, $lang)
       $page.lastUpdated = lastUpdated
     }
   }
 })
+
+function defaultTransformer (timestamp, lang) {
+  return new Date(timestamp).toLocaleString(lang)
+}
 
 function getGitLastUpdatedTimeStamp (filePath) {
   let lastUpdated
