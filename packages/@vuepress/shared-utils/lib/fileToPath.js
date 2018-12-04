519155244bc44d7b029730b121d015da69df64eb
Daniel Castillo <dcastillok@gmail.com>
2018-12-04 12:25:18 +0100

    🔀 Resolve conflicts with master

--- packages/@vuepress/shared-utils/lib/fileToPath.js
+++ packages/@vuepress/shared-utils/lib/fileToPath.js
@@ -6,15 +6,21 @@ const extRE = /\.(vue|md)$/
 module.exports = function fileToPath (file, htmlSuffix) {
   if (isIndexFile(file)) {
     // README.md -> /
+    // README.vue -> /
     // foo/README.md -> /foo/
+    // foo/README.vue -> /foo/
     return file.replace(indexRE, '/$1')
   } else if (htmlSuffix) {
     // foo.md -> /foo.html
+    // foo.vue -> /foo.html
     // foo/bar.md -> /foo/bar.html
+    // foo/bar.vue -> /foo/bar.html
     return `/${file.replace(extRE, '').replace(/\\/g, '/')}.html`
   } else {
     // foo.md -> /foo/
+    // foo.vue -> /foo/
     // foo/bar.md -> /foo/bar/
+    // foo/bar.vue -> /foo/bar/
     return `/${file.replace(extRE, '').replace(/\\/g, '/')}/`
   }
 }
