929da11f9c608758fdbdbec376599638dec6fcce
Dmitry Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-12-23 17:18:58 +0300

    fix($plugin-back-to-top): use $accent-color. (close: #1121) (#1122)

--- packages/@vuepress/plugin-back-to-top/BackToTop.vue
+++ packages/@vuepress/plugin-back-to-top/BackToTop.vue
@@ -59,19 +59,19 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang='stylus' scoped>
 .go-to-top {
   cursor: pointer;
   position: fixed;
   bottom: 2rem;
   right: 2.5rem;
   width: 2rem;
-  color: #72cda4;
+  color: $accentColor;
   z-index: 1;
 }
 
 .go-to-top:hover {
-  color: #3eaf7c;
+  color: lighten($accentColor, 30%);
 }
 
 @media (max-width: 959px) {
