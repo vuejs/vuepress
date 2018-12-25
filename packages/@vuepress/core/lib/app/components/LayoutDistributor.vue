b562972eb4e8a205adda386bbb31784f015c1da6
ULIVZ <472590061@qq.com>
2018-12-25 23:57:38 +0800

    fix($core): NotFound component doesn't work (close: #1130)
    BTW, load Layout component synchronously

--- packages/@vuepress/core/lib/app/components/LayoutDistributor.vue
+++ packages/@vuepress/core/lib/app/components/LayoutDistributor.vue
@@ -3,6 +3,12 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import { getLayoutAsyncComponent } from '../util'
+
+Vue.component('Layout', getLayoutAsyncComponent('Layout'))
+Vue.component('NotFound', getLayoutAsyncComponent('NotFound'))
+
 export default {
   computed: {
     layout () {
