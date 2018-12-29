vuejs/vuepress

commit 1c2aa08ed528fc084044184fe44afc854f119769

perf: improve ssr performance (#1068)
* perf: improve ssr performance and resolve memory leak

* reduce to normalized pages map

* assign and return map in reducer function

* return complete map from reduder function

packages/@vuepress/theme-default/util/index.js
@@ -58,13 +58,11 @@ export function resolvePage (pages, rawPath, base) {
     rawPath = resolvePath(rawPath, base)
   }
   const path = normalize(rawPath)
-  for (let i = 0; i < pages.length; i++) {
-    if (normalize(pages[i].regularPath) === path) {
-      return Object.assign({}, pages[i], {
-        type: 'page',
-        path: ensureExt(pages[i].path)
-      })
-    }
+  if (path in pages) {
+    return Object.assign({}, pages[path], {
+      type: 'page',
+      path: ensureExt(pages[path].path)
+    })
   }
   console.error(`[vuepress] No matching page found for sidebar item "${rawPath}"`)
   return {}
@@ -128,12 +126,16 @@ export function resolveSidebarItems (page, regularPath, site, localePath) {
   }
 
   const sidebarConfig = localeConfig.sidebar || themeConfig.sidebar
+  const normalizedPagesMap = pages.reduce((map, page) => {
+    map[normalize(page.regularPath)] = page
+    return map
+  }, {})
   if (!sidebarConfig) {
     return []
   } else {
     const { base, config } = resolveMatchingConfig(regularPath, sidebarConfig)
     return config
-      ? config.map(item => resolveItem(item, pages, base))
+      ? config.map(item => resolveItem(item, normalizedPagesMap, base))
       : []
   }
 }

commit 226fdd7283ab5593d557b89a94cb96275a271384

fix($theme-default): front matter prev / next links fixes bug introduced with #1068.

packages/@vuepress/theme-default/util/index.js
@@ -53,7 +53,18 @@ export function isActive (route, path) {
   return routePath === pagePath
 }
 
+function normalizedPagesMap (pages) {
+  return pages.reduce((map, page) => {
+    map[normalize(page.regularPath)] = page
+    return map
+  }, {})
+}
+
 export function resolvePage (pages, rawPath, base) {
+  return resolvePageByMap(normalizedPagesMap(pages), rawPath, base)
+}
+
+function resolvePageByMap (pages, rawPath, base) {
   if (base) {
     rawPath = resolvePath(rawPath, base)
   }
@@ -126,16 +137,12 @@ export function resolveSidebarItems (page, regularPath, site, localePath) {
   }
 
   const sidebarConfig = localeConfig.sidebar || themeConfig.sidebar
-  const normalizedPagesMap = pages.reduce((map, page) => {
-    map[normalize(page.regularPath)] = page
-    return map
-  }, {})
   if (!sidebarConfig) {
     return []
   } else {
     const { base, config } = resolveMatchingConfig(regularPath, sidebarConfig)
     return config
-      ? config.map(item => resolveItem(item, normalizedPagesMap, base))
+      ? config.map(item => resolveItem(item, normalizedPagesMap(pages), base))
       : []
   }
 }
@@ -211,9 +218,9 @@ function ensureEndingSlash (path) {
 
 function resolveItem (item, pages, base, isNested) {
   if (typeof item === 'string') {
-    return resolvePage(pages, item, base)
+    return resolvePageByMap(pages, item, base)
   } else if (Array.isArray(item)) {
-    return Object.assign(resolvePage(pages, item[0], base), {
+    return Object.assign(resolvePageByMap(pages, item[0], base), {
       title: item[1]
     })
   } else {