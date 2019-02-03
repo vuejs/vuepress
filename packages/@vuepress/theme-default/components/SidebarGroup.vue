ac9776d4b8e36668110ff5f5e3ebaf9fccabfe52
ULIVZ <472590061@qq.com>
2019-02-03 20:02:51 +0800

    chore: resume dropdown transition

--- packages/@vuepress/theme-default/components/SidebarGroup.vue
+++ packages/@vuepress/theme-default/components/SidebarGroup.vue
@@ -35,26 +35,29 @@
       </span>
     </p>
 
-    <ul
-      ref="items"
-      class="sidebar-group-items"
-      v-if="open || !collapsable"
-    >
-      <li v-for="child in item.children">
-        <SidebarLink :item="child"/>
-      </li>
-    </ul>
+    <DropdownTransition>
+      <ul
+        ref="items"
+        class="sidebar-group-items"
+        v-if="open || !collapsable"
+      >
+        <li v-for="child in item.children">
+          <SidebarLink :item="child"/>
+        </li>
+      </ul>
+    </DropdownTransition>
   </section>
 </template>
 
 <script>
 import { isActive } from '../util'
 import SidebarLink from './SidebarLink.vue'
+import DropdownTransition from './DropdownTransition.vue'
 
 export default {
   name: 'SidebarGroup',
   props: ['item', 'first', 'open', 'collapsable'],
-  components: { SidebarLink },
+  components: { SidebarLink, DropdownTransition },
   methods: { isActive }
 }
 </script>
