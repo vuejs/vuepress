05e793f0ba72fedb3bc96bc894824a0a4009e905
ULIVZ <472590061@qq.com>
2019-02-08 22:58:23 +0800

    feat($theme-default): highlight sidebar link text at root level like sidebar group heading text

--- packages/@vuepress/theme-default/components/Sidebar.vue
+++ packages/@vuepress/theme-default/components/Sidebar.vue
@@ -41,6 +41,10 @@ export default {
       padding 0.5rem 0 0.5rem 1.5rem
   & > .sidebar-links
     padding 1.5rem 0
+    & > li > a.sidebar-link
+      font-size 1.1em
+      line-height 1.7
+      font-weight bold
     & > li:not(:first-child)
       margin-top .75rem
 
