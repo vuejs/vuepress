5a111a2f24f09fd43200917860c1504bdb4e48db
Shigma <33423008+Shigma@users.noreply.github.com>
2019-03-03 01:34:15 +0800

    fix($markdown): fix line highlighting (close: #1364) (#1369)

--- packages/@vuepress/markdown/lib/highlightLines.js
+++ packages/@vuepress/markdown/lib/highlightLines.js
@@ -9,27 +9,29 @@ module.exports = md => {
     const [tokens, idx, options] = args
     const token = tokens[idx]
 
-    const rawInfo = token.info
-    if (!rawInfo || !RE.test(rawInfo)) {
-      return fence(...args)
+    if (!token.lineNumbers) {
+      const rawInfo = token.info
+      if (!rawInfo || !RE.test(rawInfo)) {
+        return fence(...args)
+      }
+
+      const langName = rawInfo.replace(RE, '').trim()
+      // ensure the next plugin get the correct lang.
+      token.info = langName
+
+      token.lineNumbers = RE.exec(rawInfo)[1]
+        .split(',')
+        .map(v => v.split('-').map(v => parseInt(v, 10)))
     }
 
-    const langName = rawInfo.replace(RE, '').trim()
-    // ensure the next plugin get the correct lang.
-    token.info = langName
-
-    const lineNumbers = RE.exec(rawInfo)[1]
-      .split(',')
-      .map(v => v.split('-').map(v => parseInt(v, 10)))
-
     const code = options.highlight
-      ? options.highlight(token.content, langName)
+      ? options.highlight(token.content, token.info)
       : token.content
 
     const rawCode = code.replace(wrapperRE, '')
     const highlightLinesCode = rawCode.split('\n').map((split, index) => {
       const lineNumber = index + 1
-      const inRange = lineNumbers.some(([start, end]) => {
+      const inRange = token.lineNumbers.some(([start, end]) => {
         if (start && end) {
           return lineNumber >= start && lineNumber <= end
         }
