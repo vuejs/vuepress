3edbd8b68e203d91299968311422b367021b1e8a
Xinyu Liu <meteor.lxy@foxmail.com>
2019-03-06 17:58:26 +0800

    chore: cache pascalize function

--- packages/@vuepress/core/lib/client/util.js
+++ packages/@vuepress/core/lib/client/util.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 
-function pascalize (str = '') {
-  return str.replace(/(^|-)\w/g, s => s.slice(-1).toUpperCase())
+function cached (fn) {
+  const cache = Object.create(null)
+  return str => {
+    if (typeof cache[str] === 'undefined') {
+      cache[str] = fn(str)
+    }
+    return cache[str]
+  }
 }
+
+const pascalize = cached((str = '') => str.replace(/(^|-)\w/g, s => s.slice(-1).toUpperCase()))
 
 export function isPageExists (pageKey) {
   return Boolean(Vue.component(pascalize(pageKey)))
