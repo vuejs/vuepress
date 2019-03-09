0306574b1f852e3284a65c62d2c85a1e0fefe97a
ULIVZ <472590061@qq.com>
2019-03-10 04:41:33 +0800

    fix($core): global components cannot be used as layouts (close: #1321)

--- packages/@vuepress/core/lib/client/components/GlobalLayout.vue
+++ packages/@vuepress/core/lib/client/components/GlobalLayout.vue
@@ -3,11 +3,16 @@
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   computed: {
     layout () {
       if (this.$page.path) {
-        if (this.$vuepress.isLayoutExists(this.$page.frontmatter.layout)) {
+        if (
+          this.$vuepress.isLayoutExists(this.$page.frontmatter.layout)
+          || Boolean(Vue.component(this.$page.frontmatter.layout))
+        ) {
           return this.$page.frontmatter.layout
         }
         return 'Layout'
