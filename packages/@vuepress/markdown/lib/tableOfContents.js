12ea48e8b0882c88b14fae568cdeb09ce3a24706
Shigma <1700011071@pku.edu.cn>
2019-04-07 02:16:14 +0800

    Update tableOfContents.js

--- packages/@vuepress/markdown/lib/tableOfContents.js
+++ packages/@vuepress/markdown/lib/tableOfContents.js
@@ -13,39 +13,20 @@ module.exports = (md, options) => {
   options = Object.assign({}, defaults, options)
   const tocRegexp = options.markerPattern
 
-  function toc (state, silent) {
-    var token
-    var match
-
-    // Reject if the token does not start with [
-    if (state.src.charCodeAt(state.pos) !== 0x5B /* [ */) {
-      return false
-    }
-    // Don't run any pairs in validation mode
-    if (silent) {
-      return false
-    }
-
-    // Detect TOC markdown
-    match = tocRegexp.exec(state.src)
-    match = !match ? [] : match.filter(function (m) { return m })
-    if (match.length < 1) {
-      return false
-    }
+  function toc (state, startLine) {
+    const pos = state.bMarks[startLine] + state.tShift[startLine]
+    const max = state.eMarks[startLine]
+    const match = tocRegexp.exec(state.src.slice(pos, max))
+    if (!match) return false
 
     // Build content
-    token = state.push('toc_open', 'toc', 1)
+    let token = state.push('toc_open', 'toc', 1)
     token.markup = '[[toc]]'
     token = state.push('toc_body', '', 0)
     token = state.push('toc_close', 'toc', -1)
 
     // Update pos so the parser can continue
-    var newline = state.src.indexOf('\n')
-    if (newline !== -1) {
-      state.pos = state.pos + newline
-    } else {
-      state.pos = state.pos + state.posMax + 1
-    }
+    state.line = startLine + 1
 
     return true
   }
@@ -68,7 +49,7 @@ module.exports = (md, options) => {
   }
 
   // Insert TOC
-  md.inline.ruler.after('emphasis', 'toc', toc)
+  md.block.ruler.after('fence', 'toc', toc)
 }
 
 /** escape double quotes in v-bind derivatives */
