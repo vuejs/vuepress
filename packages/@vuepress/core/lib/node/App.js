6da9a5f112fae442ca26b14374d18ca560769053
ULIVZ <472590061@qq.com>
2019-04-07 01:08:05 +0800

    feat($core): emit warning if the source directory doesn't exist (close: #1521)

--- packages/@vuepress/core/lib/node/App.js
+++ packages/@vuepress/core/lib/node/App.js
@@ -41,6 +41,9 @@ module.exports = class App {
     this.options = options
     this.sourceDir = this.options.sourceDir || path.join(__dirname, 'docs.fallback')
     logger.debug('sourceDir', this.sourceDir)
+    if (!fs.existsSync(this.sourceDir)) {
+      logger.warn(`Source directory doesn't exist: ${chalk.yellow(this.sourceDir)}`)
+    }
 
     const { tempPath, writeTemp } = createTemp(options.temp)
     this.tempPath = tempPath
