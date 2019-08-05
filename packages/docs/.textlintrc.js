26abaed7e5d1d0be0b295749c093080f11b238b7
Sarah Dayan <sarah.dayan@algolia.com>
2019-08-05 21:49:03 +0200

    feat: set up configuration file and command

---
+++ packages/docs/.textlintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  rules: {
+    '@textlint-rule/no-unmatched-pair': true,
+    'apostrophe': true,
+    'common-misspellings': true,
+    'diacritics': true,
+    'en-capitalization': {
+      'allowHeading': false
+    },
+    'stop-words': true,
+    'terminology': true,
+    'write-good': true
+  }
+}
