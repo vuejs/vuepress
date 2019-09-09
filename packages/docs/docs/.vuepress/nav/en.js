dff46e192f147240a2cb5343ae43829e22e626be
giraud florent <feltron@girauds-MacBook-Pro.local>
2019-09-09 11:43:36 -0400

    fix(format): rollback single quotes modif on navs config

--- packages/docs/docs/.vuepress/nav/en.js
+++ packages/docs/docs/.vuepress/nav/en.js
@@ -1,56 +1,56 @@
 module.exports = [
   {
-    text: "Guide",
-    link: "/guide/"
+    text: 'Guide',
+    link: '/guide/'
   },
   {
-    text: "Plugins / Themes",
-    link: "/plugin/market/official/plugin-active-header-links"
+    text: 'Plugins / Themes',
+    link: '/plugin/market/official/plugin-active-header-links'
   },
   {
-    text: "Learn More",
+    text: 'Learn More',
     items: [
       {
-        text: "Config reference",
-        link: "/config/index.html"
+        text: 'Config reference',
+        link: '/config/index.html'
       },
       {
-        text: "Cli",
-        link: "/api/cli.html"
+        text: 'Cli',
+        link: '/api/cli.html'
       },
       {
-        text: "Create a plugin",
-        link: "/plugin/index.html"
+        text: 'Create a plugin',
+        link: '/plugin/index.html'
       },
       {
-        text: "Create a theme",
-        link: "/theme/index.html"
+        text: 'Create a theme',
+        link: '/theme/index.html'
       },
       {
-        text: "NodeJS Api",
-        link: "/api/node.html"
+        text: 'NodeJS Api',
+        link: '/api/node.html'
       },
       {
-        text: "Contributing Guide",
-        link: "/miscellaneous/local-development.html"
+        text: 'Contributing Guide',
+        link: '/miscellaneous/local-development.html'
       },
       {
-        text: "Miscellaneous",
+        text: 'Miscellaneous',
         items: [
           {
-            text: "Migrate from 0.x",
-            link: "/miscellaneous/migration-guide.html"
+            text: 'Migrate from 0.x',
+            link: '/miscellaneous/migration-guide.html'
           },
           {
-            text: "Changelog",
-            link: "https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md"
+            text: 'Changelog',
+            link: 'https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md'
           }
         ]
       }
     ]
   },
   {
-    text: "0.x",
-    link: "https://vuepress.vuejs.org/"
+    text: '0.x',
+    link: 'https://vuepress.vuejs.org/'
   }
 ];
--- packages/docs/docs/.vuepress/nav/zh.js
+++ packages/docs/docs/.vuepress/nav/zh.js
@@ -1,60 +1,60 @@
 module.exports = [
   {
-    text: "指南",
-    link: "/zh/guide/"
+    text: '指南',
+    link: '/zh/guide/'
   },
   // {
   //   text: "配置",
   //   link: "/zh/config/"
   // },
   {
-    text: "插件 / 主题",
-    link: "/zh/plugin/market/official/plugin-active-header-links"
+    text: '插件 / 主题',
+    link: '/zh/plugin/market/official/plugin-active-header-links'
   },
   {
-    text: "了解更多",
+    text: '了解更多',
     items: [
       {
-        text: "Config reference",
-        link: "/zh/config/index.html"
+        text: 'Config reference',
+        link: '/zh/config/index.html'
       },
       {
-        text: "Cli",
-        link: "/zh/api/cli.html"
+        text: 'Cli',
+        link: '/zh/api/cli.html'
       },
       {
-        text: "Create a plugin",
-        link: "/zh/plugin/index.html"
+        text: 'Create a plugin',
+        link: '/zh/plugin/index.html'
       },
       {
-        text: "Create a theme",
-        link: "/zh/theme/index.html"
+        text: 'Create a theme',
+        link: '/zh/theme/index.html'
       },
       {
-        text: "NodeJS Api",
-        link: "/zh/api/node.html"
+        text: 'NodeJS Api',
+        link: '/zh/api/node.html'
       },
       {
-        text: "Contributing Guide",
-        link: "/zh/miscellaneous/local-development.html"
+        text: 'Contributing Guide',
+        link: '/zh/miscellaneous/local-development.html'
       },
       {
-        text: "Miscellaneous",
+        text: 'Miscellaneous',
         items: [
           {
-            text: "从 0.x 迁移",
-            link: "/zh/miscellaneous/migration-guide.html"
+            text: '从 0.x 迁移',
+            link: '/zh/miscellaneous/migration-guide.html'
           },
           {
-            text: "Changelog",
-            link: "https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md"
+            text: 'Changelog',
+            link: 'https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md'
           }
         ]
       }
     ]
   },
   {
-    text: "0.x",
-    link: "https://vuepress.vuejs.org/"
+    text: '0.x',
+    link: 'https://vuepress.vuejs.org/'
   }
 ];
