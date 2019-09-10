bc64a42b3b602451594f7b64a452be0f0452984f
Vinicius Julião <40807325+vrjuliao@users.noreply.github.com>
2019-09-10 12:05:58 -0300

    Fixing code formating

--- packages/@vuepress/plugin-search/SearchBox.vue
+++ packages/@vuepress/plugin-search/SearchBox.vue
@@ -13,7 +13,7 @@
       @keyup.enter="go(focusIndex)"
       @keyup.up="onUp"
       @keyup.down="onDown"
-    />
+    >
     <ul
       class="suggestions"
       v-if="showSuggestions"
@@ -23,7 +23,6 @@
       <li
         class="suggestion"
         v-for="(s, i) in suggestions"
-        :key="i"
         :class="{ focused: i === focusIndex }"
         @mousedown="go(i)"
         @mouseenter="focus(i)"
@@ -40,273 +39,229 @@
 <script>
 /* global SEARCH_MAX_SUGGESTIONS, SEARCH_PATHS */
 export default {
-  data() {
+  data () {
     return {
-      query: "",
+      query: '',
       focused: false,
       focusIndex: 0,
       placeholder: undefined
-    };
+    }
   },
 
   mounted () {
     this.placeholder = this.$site.themeConfig.searchPlaceholder || ''
   },
   computed: {
-    showSuggestions() {
-      return this.focused && this.suggestions && this.suggestions.length;
+    showSuggestions () {
+      return (
+        this.focused
+        && this.suggestions
+        && this.suggestions.length
+      )
     },
 
-    suggestions() {
-      const query = this.query.trim().toLowerCase();
+    suggestions () {
+      const query = this.query.trim().toLowerCase()
       if (!query) {
-        return;
+        return
       }
 
-      const { pages, themeConfig } = this.$site;
-      const max = themeConfig.searchMaxSuggestions || SEARCH_MAX_SUGGESTIONS;
-      const localePath = this.$localePath;
-      const matches = item =>
-        item.title && item.title.toLowerCase().indexOf(query) > -1;
-      const res = [];
+      const { pages } = this.$site
+      const max = themeConfig.searchMaxSuggestions || SEARCH_MAX_SUGGESTIONS
+      const localePath = this.$localePath
+      const matches = item => (
+        item.title
+        && item.title.toLowerCase().indexOf(query) > -1
+      )
+      const res = []
       for (let i = 0; i < pages.length; i++) {
-        if (res.length >= max) break;
-        const p = pages[i];
+        if (res.length >= max) break
+        const p = pages[i]
         // filter out results that do not match current locale
         if (this.getPageLocalePath(p) !== localePath) {
-          continue;
+          continue
         }
 
         // filter out results that do not match searchable paths
         if (!this.isSearchable(p)) {
-          continue;
+          continue
         }
 
         if (matches(p)) {
-          res.push(p);
+          res.push(p)
         } else if (p.headers) {
           for (let j = 0; j < p.headers.length; j++) {
-            if (res.length >= max) break;
-            const h = p.headers[j];
+            if (res.length >= max) break
+            const h = p.headers[j]
             if (matches(h)) {
-              res.push(
-                Object.assign({}, p, {
-                  path: p.path + "#" + h.slug,
-                  header: h
-                })
-              );
+              res.push(Object.assign({}, p, {
+                path: p.path + '#' + h.slug,
+                header: h
+              }))
             }
           }
         }
       }
-      return res;
+      return res
     },
 
     // make suggestions align right when there are not enough items
-    alignRight() {
-      const navCount = (this.$site.themeConfig.nav || []).length;
-      const repo = this.$site.repo ? 1 : 0;
-      return navCount + repo <= 2;
+    alignRight () {
+      const navCount = (this.$site.themeConfig.nav || []).length
+      const repo = this.$site.repo ? 1 : 0
+      return navCount + repo <= 2
     }
   },
 
   methods: {
-    getPageLocalePath(page) {
+    getPageLocalePath (page) {
       for (const localePath in this.$site.locales || {}) {
-        if (localePath !== "/" && page.path.indexOf(localePath) === 0) {
-          return localePath;
+        if (localePath !== '/' && page.path.indexOf(localePath) === 0) {
+          return localePath
         }
       }
-      return "/";
+      return '/'
     },
 
-    isSearchable(page) {
-      let searchPaths = SEARCH_PATHS;
+    isSearchable (page) {
+      let searchPaths = SEARCH_PATHS
 
       // all paths searchables
-      if (searchPaths === null) {
-        return true;
-      }
+      if (searchPaths === null) { return true }
 
-      searchPaths = Array.isArray(searchPaths)
-        ? searchPaths
-        : new Array(searchPaths);
+      searchPaths = Array.isArray(searchPaths) ? searchPaths : new Array(searchPaths)
 
-      return (
-        searchPaths.filter(path => {
-          return page.path.match(path);
-        }).length > 0
-      );
+      return searchPaths.filter(path => {
+        return page.path.match(path)
+      }).length > 0
     },
 
-    onUp() {
+    onUp () {
       if (this.showSuggestions) {
         if (this.focusIndex > 0) {
-          this.focusIndex--;
+          this.focusIndex--
         } else {
-          this.focusIndex = this.suggestions.length - 1;
+          this.focusIndex = this.suggestions.length - 1
         }
       }
     },
 
-    onDown() {
+    onDown () {
       if (this.showSuggestions) {
         if (this.focusIndex < this.suggestions.length - 1) {
-          this.focusIndex++;
+          this.focusIndex++
         } else {
-          this.focusIndex = 0;
+          this.focusIndex = 0
         }
       }
     },
 
-    go(i) {
+    go (i) {
       if (!this.showSuggestions) {
-        return;
+        return
       }
-      this.$router.push(this.suggestions[i].path);
-      this.query = "";
-      this.focusIndex = 0;
+      this.$router.push(this.suggestions[i].path)
+      this.query = ''
+      this.focusIndex = 0
     },
 
-    focus(i) {
-      this.focusIndex = i;
+    focus (i) {
+      this.focusIndex = i
     },
 
-    unfocus() {
-      this.focusIndex = -1;
+    unfocus () {
+      this.focusIndex = -1
     }
   }
-};
+}
 </script>
 
 <style lang="stylus">
-.search-box {
-  display: inline-block;
-  position: relative;
-  margin-right: 1rem;
+.search-box
+  display inline-block
+  position relative
+  margin-right 1rem
+  input
+    cursor text
+    width 10rem
+    height: 2rem
+    color lighten($textColor, 25%)
+    display inline-block
+    border 1px solid darken($borderColor, 10%)
+    border-radius 2rem
+    font-size 0.9rem
+    line-height 2rem
+    padding 0 0.5rem 0 2rem
+    outline none
+    transition all .2s ease
+    background #fff url(search.svg) 0.6rem 0.5rem no-repeat
+    background-size 1rem
+    &:focus
+      cursor auto
+      border-color $accentColor
+  .suggestions
+    background #fff
+    width 20rem
+    position absolute
+    top 1.5rem
+    border 1px solid darken($borderColor, 10%)
+    border-radius 6px
+    padding 0.4rem
+    list-style-type none
+    &.align-right
+      right 0
+  .suggestion
+    line-height 1.4
+    padding 0.4rem 0.6rem
+    border-radius 4px
+    cursor pointer
+    a
+      white-space normal
+      color lighten($textColor, 35%)
+      .page-title
+        font-weight 600
+      .header
+        font-size 0.9em
+        margin-left 0.25em
+    &.focused
+      background-color #f3f4f5
+      a
+        color $accentColor
 
-  input {
-    cursor: text;
-    width: 10rem;
-    height: 2rem;
-    color: lighten($textColor, 25%);
-    display: inline-block;
-    border: 1px solid darken($borderColor, 10%);
-    border-radius: 2rem;
-    font-size: 0.9rem;
-    line-height: 2rem;
-    padding: 0 0.5rem 0 2rem;
-    outline: none;
-    transition: all 0.2s ease;
-    background: #fff url('search.svg') 0.6rem 0.5rem no-repeat;
-    background-size: 1rem;
-
-    &:focus {
-      cursor: auto;
-      border-color: $accentColor;
-    }
-  }
-
-  .suggestions {
-    background: #fff;
-    width: 20rem;
-    position: absolute;
-    top: 1.5rem;
-    border: 1px solid darken($borderColor, 10%);
-    border-radius: 6px;
-    padding: 0.4rem;
-    list-style-type: none;
-
-    &.align-right {
-      right: 0;
-    }
-  }
-
-  .suggestion {
-    line-height: 1.4;
-    padding: 0.4rem 0.6rem;
-    border-radius: 4px;
-    cursor: pointer;
-
-    a {
-      white-space: normal;
-      color: lighten($textColor, 35%);
-
-      .page-title {
-        font-weight: 600;
-      }
-
-      .header {
-        font-size: 0.9em;
-        margin-left: 0.25em;
-      }
-    }
-
-    &.focused {
-      background-color: #f3f4f5;
-
-      a {
-        color: $accentColor;
-      }
-    }
-  }
-}
-
-@media (max-width: $MQNarrow) {
-  .search-box {
-    input {
-      cursor: pointer;
-      width: 0;
-      border-color: transparent;
-      position: relative;
-
-      &:focus {
-        cursor: text;
-        left: 0;
-        width: 10rem;
-      }
-    }
-  }
-}
+@media (max-width: $MQNarrow)
+  .search-box
+    input
+      cursor pointer
+      width 0
+      border-color transparent
+      position relative
+      &:focus
+        cursor text
+        left 0
+        width 10rem
 
 // Match IE11
-@media all and (-ms-high-contrast: none) {
-  .search-box input {
-    height: 2rem;
-  }
-}
+@media all and (-ms-high-contrast: none)
+  .search-box input
+    height 2rem
 
-@media (max-width: $MQNarrow) and (min-width: $MQMobile) {
-  .search-box {
-    .suggestions {
-      left: 0;
-    }
-  }
-}
+@media (max-width: $MQNarrow) and (min-width: $MQMobile)
+  .search-box
+    .suggestions
+      left 0
 
-@media (max-width: $MQMobile) {
-  .search-box {
-    margin-right: 0;
+@media (max-width: $MQMobile)
+  .search-box
+    margin-right 0
+    input
+      left 1rem
+    .suggestions
+      right 0
 
-    input {
-      left: 1rem;
-    }
-
-    .suggestions {
-      right: 0;
-    }
-  }
-}
-
-@media (max-width: $MQMobileNarrow) {
-  .search-box {
-    .suggestions {
-      width: calc(100vw - 4rem);
-    }
-
-    input:focus {
-      width: 8rem;
-    }
-  }
-}
+@media (max-width: $MQMobileNarrow)
+  .search-box
+    .suggestions
+      width calc(100vw - 4rem)
+    input:focus
+      width 8rem
 </style>
