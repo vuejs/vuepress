22f3bf06a1062fe29708aa79fe671432a7a696c3
giraud florent <feltron@girauds-MacBook-Pro.local>
2019-09-12 02:36:54 -0400

    chore(config): revert syntaxe config

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -19,29 +19,10 @@ module.exports = ctx => ({
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
-    ],
-    [
-      'link',
-      { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }
-    ],
-    [
-      'link',
-      {
-        rel: 'mask-icon',
-        href: '/icons/safari-pinned-tab.svg',
-        color: '#3eaf7c'
-      }
-    ],
-    [
-      'meta',
-      {
-        name: 'msapplication-TileImage',
-        content: '/icons/msapplication-icon-144x144.png'
-      }
-    ],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   theme: '@vuepress/vue',
@@ -87,36 +68,24 @@ module.exports = ctx => ({
   },
   plugins: [
     ['@vuepress/back-to-top', true],
-    [
-      '@vuepress/pwa',
-      {
-        serviceWorker: true,
-        updatePopup: true
-      }
-    ],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }],
     ['@vuepress/medium-zoom', true],
-    [
-      '@vuepress/google-analytics',
-      {
-        ga: 'UA-128189152-1'
-      }
-    ],
-    [
-      'container',
-      {
-        type: 'vue',
-        before: '<pre class="vue-container"><code>',
-        after: '</code></pre>'
-      }
-    ],
-    [
-      'container',
-      {
-        type: 'upgrade',
-        before: info => `<UpgradePath title="${info}">`,
-        after: '</UpgradePath>'
-      }
-    ]
+    ['@vuepress/google-analytics', {
+      ga: 'UA-128189152-1'
+    }],
+    ['container', {
+      type: 'vue',
+      before: '<pre class="vue-container"><code>',
+      after: '</code></pre>'
+    }],
+    ['container', {
+      type: 'upgrade',
+      before: info => `<UpgradePath title="${info}">`,
+      after: '</UpgradePath>'
+    }]
   ],
   extraWatchFiles: ['.vuepress/nav/en.js', '.vuepress/nav/zh.js']
 });
