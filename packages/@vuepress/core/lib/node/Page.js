7fcf1f6e4e9bec6861f192d3186efdc8c773686e
Divya Sasidharan <hello@shortdiv.com>
2019-10-02 13:43:38 -0500

    fix: Extract all headers

--- packages/@vuepress/core/lib/node/Page.js
+++ packages/@vuepress/core/lib/node/Page.js
@@ -114,7 +114,7 @@ module.exports = class Page {
         // headers
         const headers = extractHeaders(
           this._strippedContent,
-          ['h2', 'h3'],
+          ['h2', 'h3', 'h4', 'h5', 'h6'],
           markdown
         )
         if (headers.length) {
