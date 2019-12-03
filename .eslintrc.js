ba1f7c799ea5355d0c58909eebc22281314a32e8
meteorlxy <meteor.lxy@foxmail.com>
2019-12-03 19:11:12 +0800

    build: use vue recommend eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,27 @@
 module.exports = {
   root: true,
+
   extends: [
-    'plugin:vue-libs/recommended'
+    'plugin:vue-libs/recommended',
+    'plugin:vue/recommended'
   ],
+
   rules: {
     indent: ['error', 2, { MemberExpression: 'off' }],
+
     'no-undef': ['error'],
-    'operator-linebreak': ['error', 'before']
+
+    'operator-linebreak': ['error', 'before'],
+
+    'vue/match-component-file-name': [
+      'error',
+      {
+        extensions: ['js', 'vue'],
+        shouldMatchCase: false
+      }
+    ]
   },
+
   overrides: [
     {
       files: ['*.ts'],
