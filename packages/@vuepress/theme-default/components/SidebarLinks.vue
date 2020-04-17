984eef0d94c097f5b6096bcbe4a94e937c6a812a
z <916665067@qq.com>
2020-04-17 15:25:45 +0800

    make the content jump to the correct position when the hash path

--- packages/@vuepress/theme-default/components/SidebarLinks.vue
+++ packages/@vuepress/theme-default/components/SidebarLinks.vue
@@ -57,61 +57,19 @@ export default {
   },
   
   mounted() {
-    this.activationLink()
-    this.isInViewPortOfOne()
-  },
-  
-  updated: function () {
-    this.isInViewPortOfOne()
+    this.activationAnchor()
   },
 
   methods: {
-    activationLink () {
-      let subtitleName = decodeURIComponent(this.$route.fullPath)
-      if (!subtitleName || subtitleName=='') return
-
-      const subtitles = [].slice.call(document.querySelectorAll(AHL_SIDEBAR_LINK_SELECTOR))
-      for(let i = 0; i < subtitles.length; i++) {
-        if(decodeURIComponent(subtitles[i].getAttribute('href')).indexOf(subtitleName) != -1){
-          subtitles[i].click()
-          this.activationAnchor()
-          return
-        }
-      }
-    },
-    
     activationAnchor() {
       let anchors = [].slice.call(document.querySelectorAll(AHL_HEADER_ANCHOR_SELECTOR))
-                    .filter(anchor => decodeURIComponent(this.$route.fullPath).indexOf(decodeURIComponent(anchor.hash)) != -1)
-
+                      .filter(anchor => decodeURIComponent(this.$route.hash) == decodeURIComponent(anchor.hash))
       if(anchors == null || anchors.length<1 || anchors[0].offsetTop == undefined) return
 
-      setTimeout(function(){
-        window.scrollTo(0, anchors[0].offsetTop + 160)
-      },100)
-    },
-
-    isInViewPortOfOne () {
-      let siderbarScroll = document.getElementsByClassName("sidebar")[0]
-      let el = document.getElementsByClassName("active sidebar-link")[1]
-      if (el ==null || el == undefined || el.offsetTop == undefined) {
-        el = document.getElementsByClassName("active sidebar-link")[0]
-      }
-      if (el ==null || el == undefined || el.offsetTop == undefined) return
-      
-      const viewPortHeight = siderbarScroll.clientHeight || window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-      let offsetTop = el.offsetTop
-      let offsetBottom = el.offsetTop + el.offsetHeight
-      let scrollTop = siderbarScroll.scrollTop
-
-      let bottomVisible = (offsetBottom <= viewPortHeight + scrollTop)
-      if (!bottomVisible) {
-        siderbarScroll.scrollTop = (offsetBottom + 5 - viewPortHeight)
-      }
-      let topVisible = (offsetTop >= scrollTop)
-      if (!topVisible) {
-        siderbarScroll.scrollTop = (offsetTop - 5)
-      }
+      const bodyHeight = document.body.clientHeight 
+      let navHeight = document.getElementsByClassName('navbar')[0].offsetHeight
+      let contentHeight = document.getElementsByClassName('theme-default-content content__default')[0].clientHeight
+      window.scrollTo(0, anchors[0].offsetTop - (bodyHeight - contentHeight) + navHeight + 40)
     },
     
     refreshIndex () {
