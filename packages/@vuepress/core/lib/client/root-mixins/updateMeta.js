942eb58ca412d8cab8eedc062e6b45ca5de60a3c
hello Jun <qujun_1992@163.com>
2020-06-06 17:39:35 +0800

    Update updateMeta.js
    fix: question that parent node is not `head` when looping `meta` element

--- packages/@vuepress/core/lib/client/root-mixins/updateMeta.js
+++ packages/@vuepress/core/lib/client/root-mixins/updateMeta.js
@@ -62,8 +62,8 @@ export default {
 function updateMetaTags (newMetaTags, currentMetaTags) {
   if (currentMetaTags) {
     [...currentMetaTags].forEach(c => {
-       const parentIsHead = c.parentNode &&
-             c.parentNode.tagName.toLocaleUpperCase() === 'HEAD'
+       const parentIsHead = c.parentNode
+           && c.parentNode.tagName.toLocaleUpperCase() === 'HEAD'
       parentIsHead && document.head.removeChild(c)
     })
   }
