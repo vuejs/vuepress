a03e93d862119b72b10f452a0d98a8e9ec9350f8
Sergey Larionov <sergey.larionov@pm.me>
2020-08-19 22:09:11 +0700

    fix($shared-utils): replace diacritics with regex (#1855)
    Co-authored-by: Sergey Larionov <sergeylarionov@cloud.upwork.com>

---
+++ packages/@vuepress/shared-utils/__tests__/slugify.spec.ts
@@ -0,0 +1,30 @@
+import slugify from '../src/slugify'
+
+describe('slugify', () => {
+  test('should slugify', () => {
+    const asserts: Record<string, string> = {
+      'Привет': 'привет',
+      'Лед üäöß': 'лед-uaoß',
+      'hangul 가': 'hangul-가',
+      'ع': 'ع',
+      'ǆℍΩ': 'dzhω',
+      'ｶi⁹': 'カi9',
+      // ㌀ -> アパート'
+      '㌀': decodeURIComponent('%E3%82%A2%E3%83%8F%E3%82%9A%E3%83%BC%E3%83%88'),
+      '¼': '_1⁄4',
+      'ǆℍΩｶi⁹¼': 'dzhωカi91⁄4',
+      'Iлｔèｒｎåｔïｏｎɑｌíƶａｔï߀ԉ': 'iлternationɑliƶati߀ԉ',
+      'Båｃòл íｐѕùｍ ðｏɭ߀ｒ ѕïｔ ａϻèｔ âùþê ａԉᏧ߀üïｌɭê ƃëéｆ ｃｕｌρá ｆïｌèｔ ϻｉǥｎòｎ ｃｕρｉᏧａｔａｔ ｕｔ êлｉｍ ｔòлɢùê.':
+        'bacoл-ipѕum-ðoɭ߀r-ѕit-aϻet-auþe-aԉꮷ߀uilɭe-ƃeef-culρa-filet-ϻiǥnon-cuρiꮷatat-ut-eлim-toлɢue',
+      'ᴎᴑᴅᴇȷʂ': 'ᴎᴑᴅᴇȷʂ',
+      'hambúrguer': 'hamburguer',
+      'hŒllœ': 'hœllœ',
+      'Fußball': 'fußball',
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZé': 'abcdefghijklmnopqrstuvwxyze'
+    }
+
+    Object.keys(asserts).forEach(input => {
+      expect(slugify(input)).toBe(asserts[input])
+    })
+  })
+})
--- packages/@vuepress/shared-utils/src/slugify.ts
+++ packages/@vuepress/shared-utils/src/slugify.ts
@@ -1,14 +1,17 @@
 // string.js slugify drops non ascii chars so we have to
 // use a custom implementation here
-import { remove as removeDiacritics } from 'diacritics'
 
 // eslint-disable-next-line no-control-regex
 const rControl = /[\u0000-\u001f]/g
 const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’–—<>,.?/]+/g
+const rCombining = /[\u0300-\u036F]/g
 
 export = function slugify (str: string): string {
-  return removeDiacritics(str)
-  // Remove control characters
+  // Split accented characters into components
+  return str.normalize('NFKD')
+    // Remove accents
+    .replace(rCombining, '')
+    // Remove control characters
     .replace(rControl, '')
     // Replace special characters
     .replace(rSpecial, '-')
