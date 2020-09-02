318c202e34a5d8fe1300a9d8b3d3cb899aef8203
Jessica Sachs <jess@jessicasachs.io>
2020-09-02 15:56:02 -0400

    feat(): add support for specifying image width and height

--- packages/@vuepress/markdown/index.js
+++ packages/@vuepress/markdown/index.js
@@ -17,6 +17,8 @@ const snippetPlugin = require('./lib/snippet')
 const emojiPlugin = require('markdown-it-emoji')
 const anchorPlugin = require('markdown-it-anchor')
 const tocPlugin = require('markdown-it-table-of-contents')
+const imgSizePlugin = require('markdown-it-imsize')
+
 const {
   slugify: _slugify,
   parseHeaders,
@@ -33,6 +35,7 @@ module.exports = (markdown = {}) => {
     externalLinks,
     anchor,
     toc,
+    imgSize,
     plugins,
     lineNumbers,
     beforeInstantiate,
@@ -100,6 +103,14 @@ module.exports = (markdown = {}) => {
         format: parseHeaders
       }, toc)])
       .end()
+
+    .plugin(PLUGINS.IMG_SIZE)
+      .use(imgSizePlugin, [Object.assign({
+        html: true,
+        linkify: true,
+        typography: true
+      }, imgSize)])
+    .end()
 
   if (lineNumbers) {
     config
