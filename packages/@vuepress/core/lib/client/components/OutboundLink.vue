8d101192623e0df903c56500b2cde24125424fe0
Derek Pollard <d-pollard@users.noreply.github.com>
2020-09-21 09:13:35 -0500

    feat($theme-default): inform screen readers link opens in new tab/window (fix #2601) (#2603)

--- packages/@vuepress/core/lib/client/components/OutboundLink.vue
+++ packages/@vuepress/core/lib/client/components/OutboundLink.vue
@@ -1,14 +1,17 @@
-<template functional>
+<template>
   <svg
     class="icon outbound"
     xmlns="http://www.w3.org/2000/svg"
-    aria-hidden="true"
+    aria-labelledby="outbound-link-title"
+    role="img"
+    focusable="false"
     x="0px"
     y="0px"
     viewBox="0 0 100 100"
     width="15"
     height="15"
   >
+    <title id="outbound-link-title">{{ openInNewWindowTitle }}</title>
     <path
       fill="currentColor"
       d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
@@ -19,6 +22,16 @@
     />
   </svg>
 </template>
+
+<script>
+export default {
+  computed: {
+    openInNewWindowTitle () {
+      return this.$themeLocaleConfig.openNewWindowText || '(opens new window)'
+    }
+  }
+}
+</script>
 
 <style>
 .icon.outbound {
