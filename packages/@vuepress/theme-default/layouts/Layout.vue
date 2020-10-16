fb1d413199835e6b6127d4e725a0ce84daa2737a
meteorlxy <meteor.lxy@foxmail.com>
2020-10-16 20:46:32 +0800

    feat(theme-default): default theme skeleton

---
+++ packages/@vuepress/theme-default/layouts/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="theme-container">
+    <h1>Layout</h1>
+
+    <Content />
+
+    <Debug />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { usePageData } from '@vuepress/client'
+
+export default defineComponent({
+  name: 'Home',
+
+  setup() {
+    const { frontmatter } = usePageData()
+    const data = frontmatter
+    const actionLink = computed(() => ({
+      link: data.value.actionLink,
+      text: data.value.actionText,
+    }))
+    return {
+      data,
+      actionLink,
+    }
+  },
+})
+</script>
--- packages/@vuepress/theme-default/src/index.ts
+++ packages/@vuepress/theme-default/src/index.ts
@@ -1,1 +1,10 @@
-export * from './types'
+import { Theme } from '@vuepress/core'
+import { path } from '@vuepress/utils'
+
+const theme: Theme = {
+  name: '@vuepress/theme-default',
+
+  layouts: path.resolve(__dirname, '../layouts'),
+}
+
+export = theme
