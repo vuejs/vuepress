e1d663b4ece27012c72383f5eecb90c5fb2195ef
meteorlxy <meteor.lxy@foxmail.com>
2020-10-17 19:53:50 +0800

    feat(client): add siteLocale in debug component

--- packages/@vuepress/client/src/components/Debug.ts
+++ packages/@vuepress/client/src/components/Debug.ts
@@ -1,12 +1,13 @@
 import { h, toDisplayString, toRefs } from 'vue'
 import type { ComponentOptions } from 'vue'
 import { useRoute } from 'vue-router'
-import { useSiteData, usePageData } from '../injections'
+import { usePageData, useSiteData, useSiteLocaleData } from '../injections'
 
 export const Debug: ComponentOptions = {
   setup() {
     const { path, name, hash, fullPath } = toRefs(useRoute())
     const site = useSiteData()
+    const siteLocale = useSiteLocaleData()
     const page = usePageData()
 
     return () =>
@@ -26,6 +27,7 @@ export const Debug: ComponentOptions = {
             })}`
           ),
           h('pre', `site ${toDisplayString(site.value)}`),
+          h('pre', `siteLocale ${toDisplayString(siteLocale.value)}`),
           h('pre', `page ${toDisplayString(page.value)}`),
         ]
       )
