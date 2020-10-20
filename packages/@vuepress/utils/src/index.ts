203d58d8d97c6a3158d4a783188ce2b64e54f19c
meteorlxy <meteor.lxy@foxmail.com>
2020-10-20 15:59:29 +0800

    feat(utils): add logger and debug utils

--- packages/@vuepress/utils/src/index.ts
+++ packages/@vuepress/utils/src/index.ts
@@ -1,10 +1,12 @@
+import * as debug from 'debug'
 import * as chalk from 'chalk'
 import * as fs from 'fs-extra'
 import * as globby from 'globby'
 import * as path from 'upath'
 import hash = require('hash-sum')
-export { chalk, fs, globby, hash, path }
+export { debug, chalk, fs, globby, hash, path }
 
+export * from './logger'
 export * from './renderHead'
 export * from './renderHeadAttrs'
 export * from './requireResolve'
---
+++ packages/@vuepress/utils/src/logger.ts
@@ -0,0 +1,29 @@
+import * as chalk from 'chalk'
+
+export const info = (...args: any[]): void => {
+  console.log(chalk.cyan('info'), ...args)
+}
+
+export const tip = (...args: any[]): void => {
+  console.log(chalk.blue('tip'), ...args)
+}
+
+export const success = (...args: any[]): void => {
+  console.log(chalk.green('success'), ...args)
+}
+
+export const warn = (...args: any[]): void => {
+  console.warn(chalk.yellow('warning'), ...args)
+}
+
+export const error = (...args: any[]): void => {
+  console.error(chalk.red('error'), ...args)
+}
+
+export const logger = {
+  info,
+  tip,
+  success,
+  warn,
+  error,
+}
