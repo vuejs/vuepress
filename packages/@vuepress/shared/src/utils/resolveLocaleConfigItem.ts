3e230fd72020ba2e2e9d9c0fef90f035410c184f
meteorlxy <meteor.lxy@foxmail.com>
2020-10-21 15:17:14 +0800

    fix(shared): missing type inference for resolveLocaleConfigItem

--- packages/@vuepress/shared/src/utils/resolveLocaleConfigItem.ts
+++ packages/@vuepress/shared/src/utils/resolveLocaleConfigItem.ts
@@ -4,10 +4,10 @@ import { resolveLocalePath } from './resolveLocalePath'
 /**
  * Resolve the matched locale config item of route path
  */
-export const resolveLocaleConfigItem = (
-  locales: LocaleConfig,
+export const resolveLocaleConfigItem = <T extends Record<string, any>>(
+  locales: LocaleConfig<T>,
   routePath: string
-): LocaleConfigItem => {
+): LocaleConfigItem<T> => {
   const localePath = resolveLocalePath(locales, routePath)
   return locales[localePath] ?? {}
 }
