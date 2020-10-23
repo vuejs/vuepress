4a67da813dbae02b7bad30a944cb965b55166da5
meteorlxy <meteor.lxy@foxmail.com>
2020-10-23 15:39:02 +0800

    chore: add zh locales config in docs

--- packages/docs/src/.vuepress/config.ts
+++ packages/docs/src/.vuepress/config.ts
@@ -18,7 +18,18 @@ const config: UserConfig<DefaultThemeOptions> = {
   themeConfig: {
     locales: {
       '/': {},
-      '/zh/': {},
+      '/zh/': {
+        info: '提示',
+        warning: '注意',
+        danger: '警告',
+        notFound: [
+          '这里什么都没有',
+          '我们怎么到这来了？',
+          '这是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+      },
     },
   },
 }
