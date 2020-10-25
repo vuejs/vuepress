b77bb29e5832a56cc6860567b3d7381a1def6ec0
meteorlxy <meteor.lxy@foxmail.com>
2020-10-25 19:07:26 +0800

    feat(theme-default): migrate Badge component

--- packages/@vuepress/theme-default/src/clientAppEnhance.ts
+++ packages/@vuepress/theme-default/src/clientAppEnhance.ts
@@ -1,4 +1,5 @@
 import type { ClientAppEnhance } from '@vuepress/client'
+import Badge from './components/global/Badge.vue'
 import CodeGroup from './components/global/CodeGroup.vue'
 import CodeGroupItem from './components/global/CodeGroupItem.vue'
 import OutboundLink from './components/global/OutboundLink.vue'
@@ -7,6 +8,7 @@ import './styles/index.styl'
 
 const clientAppEnhance: ClientAppEnhance = ({ app }) => {
   /* eslint-disable vue/match-component-file-name */
+  app.component('Badge', Badge)
   app.component('CodeGroup', CodeGroup)
   app.component('CodeGroupItem', CodeGroupItem)
   app.component('OutboundLink', OutboundLink)
---
+++ packages/@vuepress/theme-default/src/components/global/Badge.vue
@@ -0,0 +1,59 @@
+<template>
+  <span
+    class="badge"
+    :class="type"
+    :style="{
+      verticalAlign: vertical,
+    }"
+  >
+    <slot>{{ text }}</slot>
+  </span>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Badge',
+
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'tip',
+    },
+    text: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    vertical: {
+      type: String,
+      required: false,
+      default: 'top',
+    },
+  },
+})
+</script>
+
+<style lang="stylus">
+@require('../../styles/config')
+
+.badge
+  display inline-block
+  font-size 14px
+  height 18px
+  line-height 18px
+  border-radius 3px
+  padding 0 6px
+  color white
+  background-color #42b983
+  &.tip, &.green
+    background-color $badgeTipColor
+  &.error
+    background-color $badgeErrorColor
+  &.warning, &.warn, &.yellow
+    background-color $badgeWarningColor
+  & + &
+    margin-left 5px
+</style>
