f67b4d3f62fee077f947362c975c08c81470b0b0
meteorlxy <meteor.lxy@foxmail.com>
2020-10-26 17:13:43 +0800

    feat(utils): add ora util

--- packages/@vuepress/utils/src/index.ts
+++ packages/@vuepress/utils/src/index.ts
@@ -2,9 +2,10 @@ import * as debug from 'debug'
 import * as chalk from 'chalk'
 import * as fs from 'fs-extra'
 import * as globby from 'globby'
+import * as ora from 'ora'
 import * as path from 'upath'
 import hash = require('hash-sum')
-export { debug, chalk, fs, globby, hash, path }
+export { debug, chalk, fs, globby, hash, ora, path }
 
 export * from './logger'
 export * from './renderHead'
