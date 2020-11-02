6b5d999fca1c227f33f4463d6c40403277fceb13
meteorlxy <meteor.lxy@foxmail.com>
2020-11-02 23:36:02 +0800

    feat(vuepress): re-export sub packages

---
+++ packages/vuepress/src/index.ts
@@ -0,0 +1,17 @@
+export * from '@vuepress/cli'
+export * from '@vuepress/core'
+
+export type {
+  BundlerWebpackOptions,
+  LoaderOptions,
+  StylePreprocessorOptions,
+  PostcssLoaderOptions,
+  LessLoaderOptions,
+  SassLoaderOptions,
+  StylusLoaderOptions,
+  WebpackDevServer,
+} from '@vuepress/bundler-webpack'
+export type {
+  DefaultThemeOptions,
+  DefaultThemeLocaleData,
+} from '@vuepress/theme-default'
