af1950784fb17c258a4c07dbaf7439cf0ec2c762
meteorlxy <meteor.lxy@foxmail.com>
2020-11-22 00:30:49 +0800

    feat(utils): disable withSpinner if DEBUG env is set

--- packages/@vuepress/utils/src/withSpinner.ts
+++ packages/@vuepress/utils/src/withSpinner.ts
@@ -3,6 +3,10 @@ import * as ora from 'ora'
 export const withSpinner = (msg: string) => async <T>(
   target: () => Promise<T>
 ): Promise<T> => {
+  if (process.env.DEBUG) {
+    return target()
+  }
+
   const spinner = ora()
   try {
     spinner.start(msg)
