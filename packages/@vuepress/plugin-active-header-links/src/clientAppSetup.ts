9076c17aee1f595a137df4ca806ee0179b6db9de
meteorlxy <meteor.lxy@foxmail.com>
2020-11-22 18:33:17 +0800

    chore(plugin-active-header-links): disable in SSR

--- packages/@vuepress/plugin-active-header-links/src/clientAppSetup.ts
+++ packages/@vuepress/plugin-active-header-links/src/clientAppSetup.ts
@@ -1,11 +1,14 @@
 import type { ClientAppSetup } from '@vuepress/client'
 import { useActiveHeaderLinks } from './composables'
 
+declare const __SSR__: boolean
 declare const AHL_HEADER_LINK_SELECTOR: string
 declare const AHL_HEADER_ANCHOR_SELECTOR: string
 declare const AHL_DELAY: number
 
 const clientAppSetup: ClientAppSetup = () => {
+  if (__SSR__) return
+
   useActiveHeaderLinks({
     headerLinkSelector: AHL_HEADER_LINK_SELECTOR,
     headerAnchorSelector: AHL_HEADER_ANCHOR_SELECTOR,
