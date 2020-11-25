d8874a097acd87189950f87b90e03c78035b1f2d
meteorlxy <meteor.lxy@foxmail.com>
2020-11-26 00:22:05 +0800

    refactor(theme-default): use isLinkHttp to determine repo type

--- packages/@vuepress/theme-default/src/utils/resolveRepoType.ts
+++ packages/@vuepress/theme-default/src/utils/resolveRepoType.ts
@@ -1,9 +1,9 @@
-import { isLinkExternal } from '@vuepress/shared'
+import { isLinkHttp } from '@vuepress/shared'
 
 export type RepoType = 'GitHub' | 'GitLab' | 'Bitbucket' | null
 
 export const resolveRepoType = (repo: string): RepoType => {
-  if (!isLinkExternal(repo)) return 'GitHub'
+  if (!isLinkHttp(repo)) return 'GitHub'
   if (/bitbucket\.org/.test(repo)) return 'Bitbucket'
   if (/gitlab\.com/.test(repo)) return 'GitLab'
   return null
