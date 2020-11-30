9e4e6233ce988bf3b6ebbbb1bc60f83a26f6709c
meteorlxy <meteor.lxy@foxmail.com>
2020-11-30 14:58:53 +0800

    fix(client): do not prepend base to http url

--- packages/@vuepress/client/src/utils/withBase.ts
+++ packages/@vuepress/client/src/utils/withBase.ts
@@ -1,9 +1,11 @@
+import { isLinkHttp } from '@vuepress/shared'
 import { useSiteData } from '../injections'
 
 /**
  * Prefix url with site base
  */
 export const withBase = (url: string): string => {
+  if (isLinkHttp(url)) return url
   const base = useSiteData().value.base
   return `${base}${url}`.replace(/\/+/, '/')
 }
