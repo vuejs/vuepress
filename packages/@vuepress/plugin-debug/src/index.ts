63935f9075838c94a361e6d232a311059bef40e8
meteorlxy <meteor.lxy@foxmail.com>
2020-12-09 23:07:15 +0800

    refactor(plugin-debug): use path util

--- packages/@vuepress/plugin-debug/src/index.ts
+++ packages/@vuepress/plugin-debug/src/index.ts
@@ -1,12 +1,12 @@
-import { resolve } from 'path'
 import type { Plugin } from '@vuepress/core'
+import { path } from '@vuepress/utils'
 
 export type DebugPluginOptions = Record<never, never>
 
 export const debugPlugin: Plugin<DebugPluginOptions> = {
   name: '@vuepress/plugin-debug',
 
-  clientAppRootComponentFiles: resolve(__dirname, './components/Debug.js'),
+  clientAppRootComponentFiles: path.resolve(__dirname, './components/Debug.js'),
 }
 
 export default debugPlugin
