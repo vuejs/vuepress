03a658364d8c5f0b2510e10cd2bf8ec8bcbf41cb
meteorlxy <meteor.lxy@foxmail.com>
2021-03-13 17:02:41 +0800

    feat(vuepress-vite): add vuepress-vite package

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -6,6 +6,6 @@ const packages = fs.readdirSync(path.resolve(__dirname, 'packages/@vuepress'))
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['vuepress', ...packages]],
+    'scope-enum': [2, 'always', ['vuepress', 'vuepress-vite', ...packages]],
   },
 }
---
+++ packages/vuepress-vite/src/index.ts
@@ -0,0 +1,9 @@
+export * from '@vuepress/cli'
+export * from '@vuepress/core'
+
+export type { ViteBundlerOptions } from '@vuepress/bundler-vite'
+
+export type {
+  DefaultThemeOptions,
+  DefaultThemeLocaleData,
+} from '@vuepress/theme-default'
