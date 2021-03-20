638ad8afdf9f3fe779e9eb1d02dca6c1caef0307
meteorlxy <meteor.lxy@foxmail.com>
2021-03-20 22:28:13 +0800

    feat(plugin-prismjs): add prismjs plugin

---
+++ packages/@vuepress/plugin-prismjs/src/loadLanguages.ts
@@ -0,0 +1,7 @@
+import * as loadLanguages from 'prismjs/components/index'
+
+// prevent warning messages
+// eslint-disable-next-line no-import-assign
+loadLanguages.silent = true
+
+export { loadLanguages }
---
+++ packages/@vuepress/plugin-prismjs/src/resolveHighlighter.ts
@@ -0,0 +1,46 @@
+import * as Prism from 'prismjs'
+import { loadLanguages } from './loadLanguages'
+
+// documentation language of corresponding language
+const docLangMap = {
+  csharp: 'xml-doc',
+  fsharp: 'xml-doc',
+  java: 'javadoc',
+  javascript: 'jsdoc',
+  php: 'phpdoc',
+  typescript: 'jsdoc',
+}
+
+export type Highlighter = (code: string) => string
+
+/**
+ * Resolve syntax highlighter for corresponding language
+ */
+export const resolveHighlighter = (lang: string): Highlighter | null => {
+  // get the languages that need to be loaded
+  const langsToLoad: string[] = []
+
+  // current language
+  if (!Prism.languages[lang]) {
+    langsToLoad.push(lang)
+  }
+
+  // doc language of current language
+  const docLang = docLangMap[lang]
+  if (docLang && !Prism.languages[docLang]) {
+    langsToLoad.push(docLang)
+  }
+
+  // try to load languages if necessary
+  if (langsToLoad.length) {
+    loadLanguages(langsToLoad)
+  }
+
+  // return null if current language could not be loaded
+  // the doc language is not required so we don't check it here
+  if (!Prism.languages[lang]) {
+    return null
+  }
+
+  return (code) => Prism.highlight(code, Prism.languages[lang], lang)
+}
