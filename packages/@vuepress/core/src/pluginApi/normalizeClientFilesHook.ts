de7d9edd9694b9548464e7e9894068523a83a19a
meteorlxy <meteor.lxy@foxmail.com>
2021-04-09 22:58:05 +0800

    chore(core): log error message before throwing

--- packages/@vuepress/core/src/pluginApi/normalizeClientFilesHook.ts
+++ packages/@vuepress/core/src/pluginApi/normalizeClientFilesHook.ts
@@ -1,5 +1,5 @@
 import { isArray, isFunction } from '@vuepress/shared'
-import { fs } from '@vuepress/utils'
+import { fs, logger } from '@vuepress/utils'
 import type { ClientFilesHook } from '../types'
 
 /**
@@ -22,7 +22,9 @@ export const normalizeClientFilesHook = (
     if (isExisted) {
       result.push(filePath)
     } else {
-      throw new Error(`file does not exist: ${filePath}`)
+      const message = `file does not exist: ${filePath}`
+      logger.error(message)
+      throw new Error(message)
     }
   }
 
