b23ed1861f4425e1b9f1f92d22ef2cfb5def2157
meteorlxy <meteor.lxy@foxmail.com>
2021-04-21 15:55:15 +0800

    test(plugin-palette): add unit tests

---
+++ packages/@vuepress/plugin-palette/__tests__/node/preparePaletteFile.spec.ts
@@ -0,0 +1,67 @@
+import { createApp } from '@vuepress/core'
+import { preparePaletteFile, presetOptions } from '@vuepress/plugin-palette'
+import { fs, path } from '@vuepress/utils'
+
+const source = path.resolve(__dirname, 'fake-source')
+const temp = path.resolve(__dirname, '../__fixtures__/.temp')
+const app = createApp({
+  source,
+  temp,
+})
+
+describe('plugin-palette > node > preparePaletteFile', () => {
+  describe('should generate palette temp files correctly', () => {
+    const testCases = [
+      {
+        name: 'css',
+        ext: 'css',
+      },
+      {
+        name: 'sass',
+        ext: 'scss',
+      },
+      {
+        name: 'less',
+        ext: 'less',
+      },
+      {
+        name: 'stylus',
+        ext: 'styl',
+      },
+    ]
+
+    testCases.forEach(({ name, ext }) =>
+      it(`${name}`, async () => {
+        const userPaletteFile = path.resolve(
+          __dirname,
+          '../__fixtures__',
+          name,
+          `palette.${ext}`
+        )
+        const { tempPaletteFile, importCode } = presetOptions[name]
+        const tempFile = await preparePaletteFile(app, {
+          userPaletteFile,
+          tempPaletteFile,
+          importCode,
+        })
+        const result = (await fs.readFile(tempFile)).toString()
+        expect(result).toEqual(importCode(userPaletteFile))
+      })
+    )
+  })
+
+  it('should generate empty palette temp file', async () => {
+    const userPaletteFile = path.resolve(
+      __dirname,
+      '../__fixtures__',
+      'non-existent.css'
+    )
+    const tempFile = await preparePaletteFile(app, {
+      userPaletteFile,
+      tempPaletteFile: 'styles/non-existent-palette.css',
+      importCode: presetOptions.css.importCode,
+    })
+    const result = (await fs.readFile(tempFile)).toString()
+    expect(result).toEqual('')
+  })
+})
---
+++ packages/@vuepress/plugin-palette/__tests__/node/prepareStyleFile.spec.ts
@@ -0,0 +1,67 @@
+import { createApp } from '@vuepress/core'
+import { prepareStyleFile, presetOptions } from '@vuepress/plugin-palette'
+import { fs, path } from '@vuepress/utils'
+
+const source = path.resolve(__dirname, 'fake-source')
+const temp = path.resolve(__dirname, '../__fixtures__/.temp')
+const app = createApp({
+  source,
+  temp,
+})
+
+describe('plugin-palette > node > prepareStyleFile', () => {
+  describe('should generate style temp files correctly', () => {
+    const testCases = [
+      {
+        name: 'css',
+        ext: 'css',
+      },
+      {
+        name: 'sass',
+        ext: 'scss',
+      },
+      {
+        name: 'less',
+        ext: 'less',
+      },
+      {
+        name: 'stylus',
+        ext: 'styl',
+      },
+    ]
+
+    testCases.forEach(({ name, ext }) =>
+      it(`${name}`, async () => {
+        const userStyleFile = path.resolve(
+          __dirname,
+          '../__fixtures__',
+          name,
+          `index.${ext}`
+        )
+        const { tempStyleFile, importCode } = presetOptions[name]
+        const tempFile = await prepareStyleFile(app, {
+          userStyleFile,
+          tempStyleFile,
+          importCode,
+        })
+        const result = (await fs.readFile(tempFile)).toString()
+        expect(result).toEqual(importCode(userStyleFile))
+      })
+    )
+  })
+
+  it('should generate empty style temp file', async () => {
+    const userStyleFile = path.resolve(
+      __dirname,
+      '../__fixtures__',
+      'non-existent.css'
+    )
+    const tempFile = await prepareStyleFile(app, {
+      userStyleFile,
+      tempStyleFile: 'styles/non-existent-style.css',
+      importCode: presetOptions.css.importCode,
+    })
+    const result = (await fs.readFile(tempFile)).toString()
+    expect(result).toEqual('')
+  })
+})
