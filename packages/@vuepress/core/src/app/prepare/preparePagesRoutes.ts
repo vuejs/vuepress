d926a170ee5f384845f5b166029fbc392f51dcde
meteorlxy <meteor.lxy@foxmail.com>
2021-04-28 18:51:53 +0800

    perf(core): reduce routes file size

--- packages/@vuepress/core/src/app/prepare/preparePagesRoutes.ts
+++ packages/@vuepress/core/src/app/prepare/preparePagesRoutes.ts
@@ -1,6 +1,13 @@
 import { ensureLeadingSlash } from '@vuepress/shared'
 import type { App } from '../../types'
 
+type RouteItem = [
+  name: string,
+  path: string,
+  title: string,
+  redirects: string[]
+]
+
 /**
  * Generate routes temp file
  */
@@ -8,66 +15,72 @@ export const preparePagesRoutes = async (app: App): Promise<void> => {
   const content = `\
 import { Vuepress } from '@vuepress/client/lib/components/Vuepress'
 
-export const pagesRoutes = [\
+const routeItems = [\
 ${app.pages
   .map(({ key, path, pathInferred, filePathRelative, title }) => {
-    const routes: string[] = []
-
-    // add page route
-    routes.push(`
-  {
-    name: ${JSON.stringify(key)},
-    path: ${JSON.stringify(path)},
-    component: Vuepress,
-    meta: { title: ${JSON.stringify(title)} },
-  },`)
+    const redirects: string[] = []
+    const routeItem: RouteItem = [key, path, title, redirects]
 
     // paths that should redirect to this page
-    const redirects = new Set<string>()
+    const redirectsSet = new Set<string>()
 
     // redirect from decoded path
-    redirects.add(decodeURI(path))
+    redirectsSet.add(decodeURI(path))
 
     // redirect from index path
     if (/\/$/.test(path)) {
       const indexPath = path + 'index.html'
-      redirects.add(indexPath)
+      redirectsSet.add(indexPath)
     }
 
     // redirect from inferred path
     if (pathInferred !== null) {
-      redirects.add(pathInferred)
-      redirects.add(encodeURI(pathInferred))
+      redirectsSet.add(pathInferred)
+      redirectsSet.add(encodeURI(pathInferred))
     }
 
     // redirect from filename path
     if (filePathRelative !== null) {
       const filenamePath = ensureLeadingSlash(filePathRelative)
-      redirects.add(filenamePath)
-      redirects.add(encodeURI(filenamePath))
+      redirectsSet.add(filenamePath)
+      redirectsSet.add(encodeURI(filenamePath))
     }
 
     // avoid redirect from the page path itself
-    redirects.delete(path)
+    redirectsSet.delete(path)
 
-    // add redirect routes
-    redirects.forEach((redirect) => {
-      routes.push(`
-  {
-    path: ${JSON.stringify(redirect)},
-    redirect: ${JSON.stringify(path)},
-  },`)
-    })
+    // add redirects to route item
+    redirects.push(...redirectsSet)
 
-    return routes.join('')
+    return `\n  ${JSON.stringify(routeItem)},`
   })
   .join('')}
-  {
-    name: "404",
-    path: "/:catchAll(.*)",
-    component: Vuepress,
-  },
 ]
+
+export const pagesRoutes = routeItems.reduce(
+  (result, [name, path, title, redirects]) => {
+    result.push(
+      {
+        name,
+        path,
+        component: Vuepress,
+        meta: { title },
+      },
+      ...redirects.map((item) => ({
+        path: item,
+        redirect: path,
+      }))
+    )
+    return result
+  },
+  [
+    {
+      name: "404",
+      path: "/:catchAll(.*)",
+      component: Vuepress,
+    }
+  ]
+)
 `
 
   await app.writeTemp('internal/pagesRoutes.js', content)
