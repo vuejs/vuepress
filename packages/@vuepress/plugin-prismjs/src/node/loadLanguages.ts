c81f5b4f5c066df85e52b092180ac3aec3bdb926
meteorlxy <meteor.lxy@foxmail.com>
2021-06-08 00:15:04 +0800

    build: bump dependencies

--- packages/@vuepress/plugin-prismjs/src/node/loadLanguages.ts
+++ packages/@vuepress/plugin-prismjs/src/node/loadLanguages.ts
@@ -2,6 +2,7 @@ import * as Prism from 'prismjs'
 import * as rawLoadLanguages from 'prismjs/components/index'
 
 // prevent warning messages
+// @ts-ignore ts wrongly report it as a read-only property
 // eslint-disable-next-line no-import-assign
 rawLoadLanguages.silent = true
 
