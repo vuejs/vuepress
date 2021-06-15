36babba394eccc070838c6d0861e91924d305e26
meteorlxy <meteor.lxy@foxmail.com>
2021-06-15 19:59:54 +0800

    fix(bundler-vite): resolve client entry url correctly (close #190)
    resolve url differently when client entry file is outsite or inside vite root

---
+++ packages/@vuepress/bundler-vite/src/plugin/createVuepressPlugin.ts
@@ -0,0 +1,169 @@
+import type { Plugin, ResolvedConfig } from 'vite'
+import type { App } from '@vuepress/core'
+import { ensureLeadingSlash } from '@vuepress/shared'
+import { fs, path } from '@vuepress/utils'
+import type { ViteBundlerOptions } from '../types'
+import { resolveAlias } from './resolveAlias'
+import { resolveDefine } from './resolveDefine'
+
+// packages that include client code, which should not
+// be optimized nor externalized
+const clientPackages = [
+  '@vuepress/client',
+  '@vuepress/plugin-active-header-links',
+  '@vuepress/plugin-back-to-top',
+  '@vuepress/plugin-container',
+  '@vuepress/plugin-debug',
+  '@vuepress/plugin-docsearch',
+  '@vuepress/plugin-git',
+  '@vuepress/plugin-google-analytics',
+  '@vuepress/plugin-medium-zoom',
+  '@vuepress/plugin-nprogress',
+  '@vuepress/plugin-palette',
+  '@vuepress/plugin-prismjs',
+  '@vuepress/plugin-pwa',
+  '@vuepress/plugin-pwa-popup',
+  '@vuepress/plugin-register-components',
+  '@vuepress/plugin-search',
+  '@vuepress/plugin-shiki',
+  '@vuepress/plugin-theme-data',
+  '@vuepress/plugin-toc',
+  '@vuepress/theme-default',
+]
+
+export const createVuepressPlugin = ({
+  app,
+  options,
+  isServer,
+  isBuild,
+}: {
+  app: App
+  options: ViteBundlerOptions
+  isServer: boolean
+  isBuild: boolean
+}): Plugin => {
+  let resolvedConfig: ResolvedConfig
+
+  return {
+    name: 'vuepress',
+
+    config: async () => ({
+      root: app.dir.source(),
+      base: app.options.base,
+      mode: isBuild ? 'production' : 'development',
+      define: await resolveDefine({ app, isServer }),
+      publicDir: app.dir.public(),
+      cacheDir: app.dir.cache(),
+      resolve: {
+        alias: await resolveAlias({ app }),
+      },
+      css: {
+        postcss: {
+          plugins: isServer
+            ? []
+            : [
+                require('autoprefixer'),
+                ...(isBuild ? [require('postcss-csso')] : []),
+              ],
+        },
+      },
+      server: {
+        host: app.options.host,
+        port: app.options.port,
+        open: app.options.open,
+        // TODO: remove after https://github.com/vitejs/vite/issues/3373 is solved
+        fsServe: {
+          root: process.cwd(),
+        },
+      },
+      build: {
+        ssr: isServer,
+        outDir: isServer ? app.dir.dest('.server') : app.dir.dest(),
+        cssCodeSplit: false,
+        rollupOptions: {
+          input: app.dir.client('lib/app.js'),
+          preserveEntrySignatures: 'allow-extension',
+        },
+        minify: isServer ? false : !app.env.isDebug,
+      },
+      optimizeDeps: {
+        include: ['@vuepress/shared'],
+        exclude: clientPackages,
+      },
+      ssr: {
+        noExternal: clientPackages,
+      },
+    }),
+
+    configResolved: (config) => {
+      resolvedConfig = config
+    },
+
+    async configureServer(server) {
+      // here we need to import vite client script explicitly because we do not
+      // have the `index.html` file as a normal vite app
+      const viteClient = `${app.options.base}@vite/client`
+
+      // resolve the url path of the client entry file
+      const clientEntryFilePath = app.dir.client('lib/app.js')
+      const clientEntryFilePathRelative = path.relative(
+        resolvedConfig.root,
+        clientEntryFilePath
+      )
+      const clientEntry = clientEntryFilePathRelative.startsWith('../')
+        ? // if the client entry file is outside vite root
+          // the url should be something like `/@fs/path/to/node_modules/@vuepress/client/lib/app.js`
+          // use `ensureLeadingSlash` to compat with windows file path
+          `${app.options.base}@fs${ensureLeadingSlash(clientEntryFilePath)}`
+        : // if the client entry file is inside vite root
+          // the url should be something like `/node_modules/@vuepress/client/lib/app.js`
+          `${app.options.base}${clientEntryFilePathRelative}`
+
+      // resolve the module of client entry to get the timestamp
+      const clientEntryModule = await server.moduleGraph.ensureEntryFromUrl(
+        clientEntry
+      )
+
+      return () => {
+        server.middlewares.use((req, res, next) => {
+          if (req.url!.endsWith('.html')) {
+            res.statusCode = 200
+
+            // load dev template file
+            const template = fs.readFileSync(app.options.templateDev).toString()
+
+            // inject timestamp to client entry
+            const clientEntrySrc =
+              clientEntryModule.lastHMRTimestamp > 0
+                ? `${clientEntry}?t=${clientEntryModule.lastHMRTimestamp}`
+                : clientEntry
+
+            // inject client entry to dev template
+            res.end(
+              template.replace(
+                /<\/body>/,
+                `${[
+                  `<script type="module" src="${viteClient}"></script>`,
+                  `<script type="module" src="${clientEntrySrc}"></script>`,
+                ].join('')}</body>`
+              )
+            )
+            return
+          }
+          next()
+        })
+      }
+    },
+
+    generateBundle(_, bundle) {
+      // delete all asset outputs in server build
+      if (isServer) {
+        Object.keys(bundle).forEach((key) => {
+          if (bundle[key].type === 'asset') {
+            delete bundle[key]
+          }
+        })
+      }
+    },
+  }
+}
