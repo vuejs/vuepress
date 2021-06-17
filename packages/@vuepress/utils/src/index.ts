488876ecbc9931ec4ba95b5693e59f48e4bdddd6
meteorlxy <meteor.lxy@foxmail.com>
2021-06-17 15:57:41 +0800

    refactor(utils): use export star as syntax as possible

--- packages/@vuepress/utils/src/index.ts
+++ packages/@vuepress/utils/src/index.ts
@@ -1,11 +1,12 @@
 import * as debug from 'debug'
 import * as chalk from 'chalk'
-import * as fs from 'fs-extra'
 import * as globby from 'globby'
 import * as ora from 'ora'
-import * as path from 'upath'
 import hash = require('hash-sum')
-export { debug, chalk, fs, globby, hash, ora, path }
+export { debug, chalk, globby, hash, ora }
+
+export * as fs from 'fs-extra'
+export * as path from 'upath'
 
 export * from './hasExportDefault'
 export * from './logger'
